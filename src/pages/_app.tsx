49ccc6e7ab6453590fca4e25616342b8fc07a9d2
keisuke <mrtambourineman8358@gmail.com>
2021-12-21 01:01:26 +0900

    just preparation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,39 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+
+// デフォルトの breakpoints
+// https://chakra-ui.com/docs/theming/theme#breakpoints
+const breakpoints = createBreakpoints({
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+  '2xl': '96em',
+});
+
+const theme = extendTheme({
+  breakpoints,
+  // デフォルトのフォント
+  // https://chakra-ui.com/docs/theming/theme#typography
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'Georgia, serif',
+    mono: 'Menlo, monospace',
+  },
+  // デフォルトのカラーモード
+  // https://chakra-ui.com/docs/theming/theme#config
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+  },
+});
+
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
