82aa19598bc7acb83f0a34e101b91101e5d45cc9
keisuke <mrtambourineman8358@gmail.com>
2021-12-22 01:47:27 +0900

    header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,25 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Button } from '@chakra-ui/react';
 
 export default function Header(): JSX.Element {
   // TODO：左端にロゴ、右端にログアウトボタン
   return (
     <Flex h={50}>
-      <Box w={100}>ロゴ</Box>
       <Box 
-        bg="yellow" 
+        w={260}
+        pl={5}
+        pt={3}
+      >LOGO</Box>
+      <Box
+        bg="yellow"
         color="red"
         w="100%"
+        align='right'
+        pr={5}
+        pt={3}
       >
-        Hello, Next.js with Chakra UI
+        <Button colorScheme='teal' variant='link'>
+          Logout
+        </Button>
       </Box>
     </Flex>
   );
