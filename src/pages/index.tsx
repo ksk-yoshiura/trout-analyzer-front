804aeef6eec3deb1ca133fd75143aab1151da1a1
keisuke <mrtambourineman8358@gmail.com>
2021-12-22 01:10:41 +0900

    dummy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
 // pages/index.tsx
-import { Heading } from '@chakra-ui/react';
+import {
+  Stack,
+  Heading,
+  Box
+} from '@chakra-ui/react';
 
 export default function Index(): JSX.Element {
-  return <Heading color="red">Hello, Next.js with Chakra UI</Heading>;
+  return (
+    <>
+    <Heading color="white">Title</Heading>
+    <Stack spacing={10}>
+      <Box color="red">
+        Hello, Next.js with Chakra UI
+      </Box>
+      <Box color="red">
+        Hello, Next.js with Chakra UI
+      </Box>
+      <Box color="red">
+        Hello, Next.js with Chakra UI
+      </Box>
+    </Stack>
+    </>
+  );
 }
