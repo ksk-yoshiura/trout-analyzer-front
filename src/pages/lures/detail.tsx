9d0e78ed26cb24317338ddb7ada4107299a124f0
keisuke <mrtambourineman8358@gmail.com>
2021-12-28 19:58:00 +0900

    name

---
+++ src/pages/lures/detail.tsx
@@ -0,0 +1,7 @@
+import Detail from '../../components/page/lures/detail';
+
+export default function Show(): JSX.Element {
+  return (
+    <Detail />
+  );
+}
--- src/pages/lures/show.tsx
+++
@@ -1,5 +0,0 @@
-export default function Show(): JSX.Element {
-  return (
-    <></>
-  );
-}
