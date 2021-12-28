cefb27ea8b287518ea56c27759528804702f72a9
keisuke <mrtambourineman8358@gmail.com>
2021-12-28 23:13:38 +0900

    lure list

--- src/components/model/lures/LuresList.tsx
+++ src/components/model/lures/LuresList.tsx
@@ -1,53 +1,59 @@
-import { Box, Image, Badge } from '@chakra-ui/react'
+import {
+  Box,
+  Image,
+  Badge,
+  Flex,
+  Wrap,
+  WrapItem
+} from '@chakra-ui/react'
+import { LureListtMock } from './lure_list_mock'
 
 export default function LuresList(): JSX.Element {
-  const property = {
-    imageUrl: 'https://bit.ly/2Z4KKcF',
-    imageAlt: 'Rear view of modern home with pool',
-    beds: 3,
-    baths: 2,
-    title: 'Modern home in city center in the heart of historic Los Angeles',
-    formattedPrice: '$1,900.00',
-  }
 
-return (
-  <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-    <Image src={property.imageUrl} alt={property.imageAlt} />
+  return (
+    <Wrap spacing={5}>
+      {
+        LureListtMock.map((item, index) => {
+          return (
+            <WrapItem>
+              <Box w={230} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                <Image src={item.imageUrl} alt={item.imageAlt} />
 
-    <Box p='6'>
-      <Box display='flex' alignItems='baseline'>
-        <Badge borderRadius='full' px='2' colorScheme='teal'>
-          New
-        </Badge>
-        <Box
-          color='gray.500'
-          fontWeight='semibold'
-          letterSpacing='wide'
-          fontSize='xs'
-          textTransform='uppercase'
-          ml='2'
-        >
-          {property.beds} beds &bull; {property.baths} baths
-        </Box>
-      </Box>
+                <Box p='6'>
+                  <Box display='flex' alignItems='baseline'>
+                    <Badge borderRadius='full' px='2' mr={1} colorScheme='teal'>
+                      New
+                    </Badge>
+                    <Badge borderRadius='full' px='2' color='gray.500'>
+                      {item.lureType}
+                    </Badge>
+                  </Box>
+                  <Box
+                    mt='1'
+                    fontWeight='semibold'
+                    as='h4'
+                    lineHeight='tight'
+                    isTruncated
+                  >
+                    {item.name}
+                  </Box>
+                  <Box
+                    color='gray.500'
+                    fontWeight='semibold'
+                    letterSpacing='wide'
+                    fontSize='xs'
+                    textTransform='uppercase'
+                    ml='2'
+                  >
+                    added {item.createdAt}
+                  </Box>
 
-      <Box
-        mt='1'
-        fontWeight='semibold'
-        as='h4'
-        lineHeight='tight'
-        isTruncated
-      >
-        {property.title}
-      </Box>
-
-      <Box>
-        {property.formattedPrice}
-        <Box as='span' color='gray.600' fontSize='sm'>
-          / wk
-        </Box>
-      </Box>
-
-    </Box>
-  </Box>
-)}
+                </Box>
+              </Box>
+            </WrapItem>
+          )
+        })
+      }
+    </Wrap>
+  )
+}
