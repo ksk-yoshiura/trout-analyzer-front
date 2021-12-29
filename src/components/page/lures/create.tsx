293b14e9d91d31dada3eb5a445d052c7d4507bb6
keisuke <mrtambourineman8358@gmail.com>
2021-12-29 22:57:47 +0900

    form component

--- src/components/page/lures/create.tsx
+++ src/components/page/lures/create.tsx
@@ -1,7 +1,12 @@
-
+import { 
+  Flex
+} from '@chakra-ui/react'
+import LureForm from '../../model/lures/LureForm'
 
 export default function LureCreate(): JSX.Element {
   return (
-    <>hi</>
+    <Flex textAlign="center" w="100wh">
+      <LureForm />
+    </Flex>
   )
 }
