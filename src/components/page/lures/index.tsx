01b17f0f6f526085b6757b6e2e690fa3210b97ad
keisuke <mrtambourineman8358@gmail.com>
2021-12-29 20:48:43 +0900

    next link

--- src/components/page/lures/index.tsx
+++ src/components/page/lures/index.tsx
@@ -1,16 +1,20 @@
 import {
   Flex,
-  Button
+  Button,
+  Link
 } from '@chakra-ui/react'
+import NextLink from "next/link"
 import LuresList from '../../model/lures/LuresList'
 import LureTypeSelect from '../../model/lures/LureTypeSelect'
 
-export default function Index(): JSX.Element {
+export default function LureIndex(): JSX.Element {
   return (
     <>
       <Flex pb={5} justifyContent="space-between" w="90%">
         <LureTypeSelect />
-        <Button pl={10} pr={10} colorScheme='teal'>New Lures</Button>
+        <NextLink href="lures/create" passHref>
+          <Button  pl={10} pr={10} colorScheme='teal'>New Lures</Button>
+        </NextLink>
       </Flex>
       <LuresList />
     </>
