403ff33be07a6796fc6e4db14de968c296f4e202
keisuke <mrtambourineman8358@gmail.com>
2021-12-29 19:22:48 +0900

    moved property to ts file

--- src/components/model/lures/LureDetail.tsx
+++ src/components/model/lures/LureDetail.tsx
@@ -1,53 +1,48 @@
 import { Box, Image, Badge } from '@chakra-ui/react'
+import { LureDetailMock } from './lure_detail_mock'
 
 export default function LuresList(): JSX.Element {
-  const property = {
-    imageUrl: 'https://bit.ly/2Z4KKcF',
-    imageAlt: 'Rear view of modern home with pool',
-    beds: 3,
-    baths: 2,
-    title: 'Modern home in city center in the heart of historic Los Angeles',
-    formattedPrice: '$1,900.00',
-  }
+  const property = LureDetailMock
 
-return (
-  <Box maxW='sm' overflow='hidden'>
-    <Image src={property.imageUrl} alt={property.imageAlt} borderRadius='lg' />
+  return (
+    <Box maxW='sm' overflow='hidden'>
+      <Image src={property.imageUrl} alt={property.imageAlt} borderRadius='lg' />
 
-    <Box p='6'>
-      <Box display='flex' alignItems='baseline'>
-        <Badge borderRadius='full' px='2' colorScheme='teal'>
-          New
-        </Badge>
+      <Box p='6'>
+        <Box display='flex' alignItems='baseline'>
+          <Badge borderRadius='full' px='2' colorScheme='teal'>
+            New
+          </Badge>
+          <Box
+            color='gray.500'
+            fontWeight='semibold'
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
+            ml='2'
+          >
+            {property.beds} beds &bull; {property.baths} baths
+          </Box>
+        </Box>
+
         <Box
-          color='gray.500'
+          mt='1'
           fontWeight='semibold'
-          letterSpacing='wide'
-          fontSize='xs'
-          textTransform='uppercase'
-          ml='2'
+          as='h4'
+          lineHeight='tight'
+          isTruncated
         >
-          {property.beds} beds &bull; {property.baths} baths
+          {property.title}
         </Box>
-      </Box>
 
-      <Box
-        mt='1'
-        fontWeight='semibold'
-        as='h4'
-        lineHeight='tight'
-        isTruncated
-      >
-        {property.title}
-      </Box>
-
-      <Box>
-        {property.formattedPrice}
-        <Box as='span' color='gray.600' fontSize='sm'>
-          / wk
+        <Box>
+          {property.formattedPrice}
+          <Box as='span' color='gray.600' fontSize='sm'>
+            / wk
+          </Box>
         </Box>
-      </Box>
 
+      </Box>
     </Box>
-  </Box>
-)}
+  )
+}
---
+++ src/components/model/lures/lure_detail_mock.ts
@@ -0,0 +1,9 @@
+export const LureDetailMock = 
+  {
+    imageUrl: 'https://bit.ly/2Z4KKcF',
+    imageAlt: 'Rear view of modern home with pool',
+    beds: 3,
+    baths: 2,
+    title: 'Modern home in city center in the heart of historic Los Angeles',
+    formattedPrice: '$1,900.00',
+  };
