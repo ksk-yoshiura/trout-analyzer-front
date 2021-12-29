6ecd22b1f07725fbf5c05f967a52b72888a9f7c7
keisuke <mrtambourineman8358@gmail.com>
2021-12-29 19:49:14 +0900

    indent

--- src/components/page/lures/index.tsx
+++ src/components/page/lures/index.tsx
@@ -8,11 +8,11 @@ import LureTypeSelect from '../../model/lures/LureTypeSelect'
 export default function Index(): JSX.Element {
   return (
     <>
-    <Flex pb={5} justifyContent="space-between" w="90%">
-      <LureTypeSelect />
-      <Button pl={10} pr={10} colorScheme='teal'>New Lures</Button>
-    </Flex>
-    <LuresList />
+      <Flex pb={5} justifyContent="space-between" w="90%">
+        <LureTypeSelect />
+        <Button pl={10} pr={10} colorScheme='teal'>New Lures</Button>
+      </Flex>
+      <LuresList />
     </>
   );
 }
