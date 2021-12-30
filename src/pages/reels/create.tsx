ee5dd37b8195b3a64ebd10069a11413a33e2633d
keisuke <mrtambourineman8358@gmail.com>
2021-12-30 21:15:21 +0900

    pages for reel

---
+++ src/pages/reels/create.tsx
@@ -0,0 +1,7 @@
+import ReelCreate from '../../components/page/reels/create';
+
+export default function Create(): JSX.Element {
+  return (
+    <ReelCreate />
+  );
+}
---
+++ src/pages/reels/index.tsx
@@ -0,0 +1,7 @@
+import ReelIndex from '../../components/page/reels/index';
+
+export default function Index(): JSX.Element {
+  return (
+    <ReelIndex />
+  );
+}
