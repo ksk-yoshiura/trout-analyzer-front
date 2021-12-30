24606e66f169249e6abf843d0c72c3e0ad0b4e04
keisuke <mrtambourineman8358@gmail.com>
2021-12-30 22:37:26 +0900

    lines pages

---
+++ src/pages/lines/create.tsx
@@ -0,0 +1,7 @@
+import LineCreate from '../../components/page/lines/create';
+
+export default function Create(): JSX.Element {
+  return (
+    <LineCreate />
+  );
+}
---
+++ src/pages/lines/index.tsx
@@ -0,0 +1,7 @@
+import LineIndex from '../../components/page/lines/index';
+
+export default function Index(): JSX.Element {
+  return (
+    <LineIndex />
+  );
+}
