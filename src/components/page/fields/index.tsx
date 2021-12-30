ae189b39ef2841e3025a6f3075738113a0d50718
keisuke <mrtambourineman8358@gmail.com>
2021-12-30 22:23:26 +0900

    field page

---
+++ src/components/page/fields/index.tsx
@@ -0,0 +1,19 @@
+import {
+  Flex,
+  Button
+} from '@chakra-ui/react'
+import NextLink from "next/link"
+import FieldsList from '../../model/fields/FieldsList'
+
+export default function FieldIndex(): JSX.Element {
+  return (
+    <>
+      <Flex pb={5} w="90%">
+        <NextLink href="/fields/create" passHref>
+          <Button  pl={10} pr={10} colorScheme='teal'>New Field</Button>
+        </NextLink>
+      </Flex>
+      <FieldsList />
+    </>
+  );
+}
