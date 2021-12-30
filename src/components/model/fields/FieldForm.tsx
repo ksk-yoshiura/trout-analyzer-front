cfeb26c72d0eb2c04e876672c21c988b33e5b4b2
keisuke <mrtambourineman8358@gmail.com>
2021-12-30 22:20:45 +0900

    field form

--- src/components/model/fields/FieldForm.tsx
+++ src/components/model/fields/FieldForm.tsx
@@ -13,22 +13,20 @@ import {
   Stack
 } from "@chakra-ui/react";
 
-type ReelData = {
+type FieldData = {
   name: string;
-  company: string;
-  type: string;
-  gear: string;
+  place: string;
   image: string;
 }
 
 export default function ReelForm() {
-  function handleSendReelData(values: ReelData) {
+  function handleSendFieldData(values: FieldData) {
     alert(JSON.stringify(values))
 
   }
 
 
-  function validateData(value: ReelData) {
+  function validateData(value: FieldData) {
     // let error
     // if (!value) {
     //   error = 'Name is required'
@@ -42,14 +40,12 @@ export default function ReelForm() {
     <Formik
       initialValues={{
         name: '',
-        company: '',
-        gear: '',
-        type: '',
+        place: '',
         image: ''
       }}
       onSubmit={(values, actions) => {
         setTimeout(() => {
-          handleSendReelData(values)
+          handleSendFieldData(values)
           actions.setSubmitting(false)
         }, 1000)
       }}
@@ -73,50 +69,18 @@ export default function ReelForm() {
               )}
             </Field>
 
-            <Field name='gear' validate={validateData}>
+            <Field name='place' validate={validateData}>
               {({ field, form }: FieldProps) => (
                 <FormControl
-                  isInvalid={Boolean(form.errors.gear)
-                    && Boolean(form.touched.gear)}
+                  isInvalid={Boolean(form.errors.place)
+                    && Boolean(form.touched.place)}
                 >
                   <FormLabel
                     fontSize="12px"
-                    htmlFor='gear'
-                  >GEAR</FormLabel>
-                  <Input {...field} width="100%" fontSize="1xl" id='gear' placeholder='Enter' />
-                  <FormErrorMessage>{form.errors.gear}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-
-            <Field name='type' validate={validateData}>
-              {({ field, form }: FieldProps) => (
-                <FormControl
-                  isInvalid={Boolean(form.errors.type)
-                    && Boolean(form.touched.type)}
-                >
-                  <FormLabel
-                    fontSize="12px"
-                    htmlFor='type'
-                  >TYPE</FormLabel>
-                  <Input {...field} width="100%" fontSize="1xl" id='type' placeholder='Enter' />
-                  <FormErrorMessage>{form.errors.type}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-
-            <Field name='company' validate={validateData}>
-              {({ field, form }: FieldProps) => (
-                <FormControl
-                  isInvalid={Boolean(form.errors.company)
-                    && Boolean(form.touched.company)}
-                >
-                  <FormLabel
-                    fontSize="12px"
-                    htmlFor='company'
-                  >COMPANY</FormLabel>
-                  <Input {...field} width="100%" fontSize="1xl" id='company' placeholder='Enter' />
-                  <FormErrorMessage>{form.errors.company}</FormErrorMessage>
+                    htmlFor='place'
+                  >PLACE</FormLabel>
+                  <Input {...field} width="100%" fontSize="1xl" id='place' placeholder='Enter' />
+                  <FormErrorMessage>{form.errors.place}</FormErrorMessage>
                 </FormControl>
               )}
             </Field>
