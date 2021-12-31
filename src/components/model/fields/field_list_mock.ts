3a49d29ea1c2385b3e3c055ef38ca721eb0bc450
keisuke <mrtambourineman8358@gmail.com>
2021-12-31 23:15:34 +0900

    decent place name mock

--- src/components/model/fields/field_list_mock.ts
+++ src/components/model/fields/field_list_mock.ts
@@ -4,90 +4,90 @@ export const FieldListtMock = [
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '2',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '3',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '4',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '5',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '6',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '7',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '8',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '9',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '10',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '11',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '12',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   },
   {
     id: '13',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'fields image',
     lastVisited: '2021/12/28',
-    name: 'super crank bait'
+    name: 'super good fishing park'
   }
 ];
