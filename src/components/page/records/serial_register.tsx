b479052b394dd3d9ebeb44d6344eca855ce25655
keisuke <mrtambourineman8358@gmail.com>
2022-01-03 00:36:46 +0900

    serial records url

---
+++ src/components/page/records/serial_register.tsx
@@ -0,0 +1,7 @@
+import RecordSerialRegisterForm from '../../model/records/RecordSerialRegisterForm'
+
+export default function SerialRegister(): JSX.Element {
+  return (
+      <RecordSerialRegisterForm />
+  );
+}
---
+++ src/pages/records/serial_register.tsx
@@ -0,0 +1,7 @@
+import SerialRegister from '../../components/page/records/serial_register';
+
+export default function Register(): JSX.Element {
+  return (
+    <SerialRegister />
+  );
+}
