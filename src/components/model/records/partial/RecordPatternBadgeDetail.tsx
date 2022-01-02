e073f3da40764f6badbd482a828bb864eac0d6a6
keisuke <mrtambourineman8358@gmail.com>
2022-01-02 15:35:25 +0900

    partial for detail

---
+++ src/components/model/records/partial/RecordPatternBadgeDetail.tsx
@@ -0,0 +1,31 @@
+import {
+  Box,
+  Badge
+} from '@chakra-ui/react'
+
+type BadgeProps = { 
+  result: string;
+  lureType: string;
+  weather: string;
+  depth: string;
+}
+
+export default function RecordPatternBadgeDetail(props: BadgeProps): JSX.Element {
+  return (
+    <Box display='flex' alignItems='baseline'>
+      <Badge borderRadius='full' px='2' mr={1} colorScheme='teal'>
+        {props.result}
+      </Badge>
+      <Badge borderRadius='full' px='2' mr={1} color='gray.500'>
+        {props.lureType}
+      </Badge>
+      <Badge borderRadius='full' px='2' mr={1} color='gray.500'>
+        {props.weather}
+      </Badge>
+      <Badge borderRadius='full' px='2' mr={1} color='gray.500'>
+        {props.depth}
+      </Badge>
+    </Box>
+
+  )
+}
