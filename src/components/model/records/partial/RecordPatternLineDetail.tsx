8f9aec22617e9857151cc8afa6f03dfdbc6584c4
keisuke <mrtambourineman8358@gmail.com>
2022-01-02 16:20:33 +0900

    name

---
+++ src/components/model/records/partial/RecordPatternLineDetail.tsx
@@ -0,0 +1,47 @@
+import {
+  Box,
+  Stack
+} from '@chakra-ui/react'
+
+type Linerops = {
+  lineType: string;
+  lineName: string;
+  lineCompany: string;
+  lineThickness: string;
+}
+
+export default function RecordPatternLineDetail(props: Linerops) {
+  return (
+    <>
+      <Box
+        my='2'
+        fontWeight='semibold'
+        fontSize={18}
+        as='h3'
+        lineHeight='tight'
+        isTruncated
+      >
+        {props.lineName}
+      </Box>
+
+      <Stack
+        color='gray.500'
+        fontWeight='semibold'
+        letterSpacing='wide'
+        fontSize='xs'
+        ml='2'
+        spacing={1}
+      >
+        <Box textTransform='uppercase'>
+          TYPE {props.lineType}
+        </Box>
+        <Box textTransform='uppercase'>
+          THICKNESS {props.lineThickness} lb
+        </Box>
+        <Box textTransform='uppercase'>
+          COMPANY {props.lineCompany}
+        </Box>
+      </Stack>
+    </>
+  )
+}
