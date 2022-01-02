d50b21ce7d41002ca357c64ddb00d815443bc639
keisuke <mrtambourineman8358@gmail.com>
2022-01-02 23:59:46 +0900

    same as tackle

--- src/components/page/preparation/place.tsx
+++ src/components/page/preparation/place.tsx
@@ -8,11 +8,11 @@ import NextLink from "next/link"
 export default function PlaceIsReady(): JSX.Element {
   return (
     <>
-      <Flex pb={5} w="100%">
+      <Flex pt={50} w="100%" justifyContent={'center'}>
         <NextLink href="/records/serial_create" passHref>
-          <Link  pl={10} pr={10} colorScheme='teal'>Place Is Ready</Link>
+          <Link w='100%' as={'button'} color='teal'>Place Is Ready</Link>
         </NextLink>
-        <Button  pl={10} pr={10} colorScheme='teal'>New Place</Button>
+        <Button  mr={5} pl={10} pr={10} colorScheme='teal'>New Place</Button>
       </Flex>
     </>
   );
