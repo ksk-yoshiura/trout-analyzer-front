706050c77644df29f753586ea7f85a1bc1e8c367
keisuke <mrtambourineman8358@gmail.com>
2022-01-02 14:07:51 +0900

    renewal

--- src/components/model/records/record_list_mock.ts
+++ src/components/model/records/record_list_mock.ts
@@ -3,117 +3,130 @@ export const RecordListtMock = [
     id: '1',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '2',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '3',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '4',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '5',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '6',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '7',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '8',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '9',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '10',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '11',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '12',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   },
   {
     id: '13',
     imageUrl: 'https://bit.ly/2Z4KKcF',
     imageAlt: 'lure image',
-    visitedAt: '2022/01/02',
-    caughtSum: '150',
-    createdAt: '2021/12/28',
-    name: 'super great pond'
+    createdAt: '11:30',
+    result: 'caught',
+    lureType: 'spoon',
+    weather: 'sunny',
+    depth: 'shallow'
   }
 ];
