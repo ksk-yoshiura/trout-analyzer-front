e53c8bd0bc428196e328e758b4395411471ce8a0
keisuke <mrtambourineman8358@gmail.com>
2022-01-03 00:31:08 +0900

    just prepared component

---
+++ src/components/model/records/RecordSerialRegisterForm.tsx
@@ -0,0 +1,137 @@
+import {
+  Formik,
+  Form,
+  Field,
+  FieldProps
+} from 'formik';
+import {
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Stack
+} from "@chakra-ui/react";
+
+type RecordData = {
+  name: string;
+  company: string;
+  type: string;
+  gear: string;
+  image: string;
+}
+
+export default function RecordSerialRegisterForm() {
+  function handleSendRecordData(values: RecordData) {
+    alert(JSON.stringify(values))
+
+  }
+
+
+  function validateData(value: RecordData) {
+    // let error
+    // if (!value) {
+    //   error = 'Name is required'
+    // } else if (value.toLowerCase() !== 'naruto') {
+    //   error = "Jeez! You're not a fan 😱"
+    // }
+    // return error
+  }
+
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        company: '',
+        gear: '',
+        type: '',
+        image: ''
+      }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          handleSendRecordData(values)
+          actions.setSubmitting(false)
+        }, 1000)
+      }}
+    >
+      {(props) => (
+        <Form>
+          <Stack spacing={5}>
+            <Field name='name' validate={validateData}>
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isInvalid={Boolean(form.errors.name)
+                    && Boolean(form.touched.name)}
+                >
+                  <FormLabel
+                    fontSize="12px"
+                    htmlFor='name'
+                  >NAME</FormLabel>
+                  <Input {...field} width="100%" fontSize="1xl" id='name'  variant='flushed' placeholder='Enter' />
+                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+
+            <Field name='gear' validate={validateData}>
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isInvalid={Boolean(form.errors.gear)
+                    && Boolean(form.touched.gear)}
+                >
+                  <FormLabel
+                    fontSize="12px"
+                    htmlFor='gear'
+                  >GEAR</FormLabel>
+                  <Input {...field} width="100%" fontSize="1xl" id='gear'  variant='flushed' placeholder='Enter' />
+                  <FormErrorMessage>{form.errors.gear}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+
+            <Field name='type' validate={validateData}>
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isInvalid={Boolean(form.errors.type)
+                    && Boolean(form.touched.type)}
+                >
+                  <FormLabel
+                    fontSize="12px"
+                    htmlFor='type'
+                  >TYPE</FormLabel>
+                  <Input {...field} width="100%" fontSize="1xl" id='type'  variant='flushed' placeholder='Enter' />
+                  <FormErrorMessage>{form.errors.type}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+
+            <Field name='company' validate={validateData}>
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isInvalid={Boolean(form.errors.company)
+                    && Boolean(form.touched.company)}
+                >
+                  <FormLabel
+                    fontSize="12px"
+                    htmlFor='company'
+                  >COMPANY</FormLabel>
+                  <Input {...field} width="100%" fontSize="1xl" id='company'  variant='flushed' placeholder='Enter' />
+                  <FormErrorMessage>{form.errors.company}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            
+          </Stack>
+          <Button
+            mt={4}
+            colorScheme='teal'
+            isLoading={props.isSubmitting}
+            type='submit'
+          >
+            Register
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  )
+}
