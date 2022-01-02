8a8c9e4a90129bd353ef0c39e93f15add97f9932
keisuke <mrtambourineman8358@gmail.com>
2022-01-03 00:29:15 +0900

    url for register

--- src/components/page/preparation/place.tsx
+++ src/components/page/preparation/place.tsx
@@ -9,7 +9,7 @@ export default function PlaceIsReady(): JSX.Element {
   return (
     <>
       <Flex pt={50} w="100%" justifyContent={'center'}>
-        <NextLink href="/records/serial_create" passHref>
+        <NextLink href="/records/serial_register" passHref>
           <Link w='100%' as={'button'} color='teal'>Place Is Ready</Link>
         </NextLink>
         <Button  mr={5} pl={10} pr={10} colorScheme='teal'>New Place</Button>
