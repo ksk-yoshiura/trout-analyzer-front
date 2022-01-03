82a5fb0edb82ce2a6aa3e6fc6b8812179ea5a505
keisuke <mrtambourineman8358@gmail.com>
2022-01-04 01:09:42 +0900

    indent

--- src/components/page/preparation/field.tsx
+++ src/components/page/preparation/field.tsx
@@ -8,12 +8,12 @@ import PreparationFieldSelect from '../../model/preparation/PreparationFieldSele
 
 export default function FieldIsReady(): JSX.Element {
   // TODO：New Fieldで登録後登録されたfieldを選択状態にしたい
-  
+
   return (
     <>
       <Flex py={50} w="100wh" justifyContent={'center'}>
-          <PreparationFieldSelect />
-          <PreparationFieldModal />
+        <PreparationFieldSelect />
+        <PreparationFieldModal />
       </Flex>
       <NextLink href="/records/serial_register" passHref>
         <Link w='100%' as={'button'} color='teal'>Field Is Ready</Link>
