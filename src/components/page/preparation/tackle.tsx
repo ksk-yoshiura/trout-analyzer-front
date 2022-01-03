6c368c361d6c5d3f941cfaad99e75ec17bc27643
keisuke <mrtambourineman8358@gmail.com>
2022-01-04 01:00:00 +0900

    button position

--- src/components/page/preparation/tackle.tsx
+++ src/components/page/preparation/tackle.tsx
@@ -8,12 +8,12 @@ import NextLink from "next/link"
 export default function IsTackleReady(): JSX.Element {
   return (
     <>
-      <Flex pt={50} w="100%" justifyContent={'center'}>
-        <NextLink href="/preparation/field" passHref>
-          <Link w='100%' as={'button'} color='teal'>Tackle Is Ready</Link>
-        </NextLink>
+      <Flex pt={50} w="100wh" justifyContent={'center'}>
         <NextLink href="/tackles/create" passHref>
           <Button mr={5} pl={10} pr={10} colorScheme='teal'>New Tackles</Button>
+        </NextLink>
+        <NextLink href="/preparation/field" passHref>
+          <Link w='100%' as={'button'} color='teal'>Tackle Is Ready</Link>
         </NextLink>
       </Flex>
     </>
