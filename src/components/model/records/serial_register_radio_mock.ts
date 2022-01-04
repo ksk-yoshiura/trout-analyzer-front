5e147a8e267214faf27c049e5e9eaaafa8aebdac
keisuke <mrtambourineman8358@gmail.com>
2022-01-04 23:58:08 +0900

    option mock for radio

---
+++ src/components/model/records/serial_register_radio_mock.ts
@@ -0,0 +1,3 @@
+export const ResultRadiotOptionMock = [ 'caught', 'hit', 'chased']
+export const SpeedRadiotOptionMock = [ 'fast', 'mormal', 'slow', 'super-slow']
+export const DepthRadiotOptionMock = [ 'top', 'shallow', 'medium', 'deep', 'bottom']
