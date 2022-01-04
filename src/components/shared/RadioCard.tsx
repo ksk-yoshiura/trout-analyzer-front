da202676330aec85c3eb04f07437a7239a528b36
keisuke <mrtambourineman8358@gmail.com>
2022-01-04 22:35:46 +0900

    button radio under construction

---
+++ src/components/shared/RadioCard.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { 
+  Box,
+  useRadio,
+} from '@chakra-ui/react'
+
+export default function RadioCard(props:any) { // TODO：一旦anyで退避
+
+  const { getInputProps, getCheckboxProps }  = useRadio(props)
+
+  const input = getInputProps()
+  const checkbox = getCheckboxProps()
+
+  return (
+    <Box as='label'>
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor='pointer'
+        borderWidth='1px'
+        borderRadius='md'
+        boxShadow='md'
+        _checked={{
+          bg: 'teal.600',
+          color: 'white',
+          borderColor: 'teal.600',
+        }}
+        _focus={{
+          boxShadow: 'outline',
+        }}
+        px={5}
+        py={3}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  )
+}
