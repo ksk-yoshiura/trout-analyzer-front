6eb2ea528811dac4ae7871b8d8ee60386fa67c09
keisuke <mrtambourineman8358@gmail.com>
2022-01-08 19:08:47 +0900

    lure select

---
+++ src/components/model/mock/lures/lure_name_list_mock.ts
@@ -0,0 +1,67 @@
+export const LureNameListMock = [
+  {
+    id: '1',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '2',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '3',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '4',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '5',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '6',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '7',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '8',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '9',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '10',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '11',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '12',
+    color: 'red',
+    name: 'super crank bait'
+  },
+  {
+    id: '13',
+    color: 'red',
+    name: 'super crank bait'
+  }
+];
--- src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
+++ src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
@@ -1,21 +1,27 @@
+import { useState } from 'react'
 import {
   Select,
   Flex
 } from "@chakra-ui/react";
 import { LureTypeSelectMock } from '../../mock/lures/lure_type_select_mock'
+import { LureNameListMock } from '../../mock/lures/lure_name_list_mock'
 
 export default function LureSelect() {
+
+  const [lureTypeId, setLureNameList] = useState('0')
+  console.log(lureTypeId)
   function changeHandler(event: any) { // TODO：anyで一旦退避
     const { target } = event;
     if (!(target instanceof HTMLSelectElement)) {
       return; // or throw new TypeError();
     }
     const targetLureTypeId = target.value
+    // TODO：ここでルアー名リストを取得する
   }
 
   return (
-    <Flex>
-      <Select w={130} onChange={(event) => changeHandler(event)} placeholder='Lure Type'>
+    <>
+      <Select w={130} onChange={(event) => {changeHandler(event),setLureNameList(event.target.value)}} placeholder='Lure Type'>
         {
           LureTypeSelectMock.map((item, index) => {
             return (
@@ -26,6 +32,20 @@ export default function LureSelect() {
           })
         }
       </Select>
-    </Flex>
+      {
+        lureTypeId !== '0' && lureTypeId ?
+        <Select w='100wh' onChange={(event) => changeHandler(event)} placeholder='Name'>
+          {
+            LureNameListMock.map((item, index) => {
+              return (
+                <option key={index} value={item.id}>
+                  {item.name} :{item.color} 
+                </option>
+              )
+            })
+          }
+        </Select>: <></>
+      }
+    </>
   )
 }
