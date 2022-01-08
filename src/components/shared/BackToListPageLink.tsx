224b9b9a8a49244f6c975b0476751b2a62ad541d
keisuke <mrtambourineman8358@gmail.com>
2022-01-09 00:29:05 +0900

    link component

---
+++ src/components/shared/BackToListPageLink.tsx
@@ -0,0 +1,22 @@
+import {
+  Link,
+  Icon
+} from '@chakra-ui/react'
+import { ArrowBackIcon } from '@chakra-ui/icons'
+import NextLink from "next/link"
+
+type PageName = {
+  name: string;
+}
+export default function BackToListPageLink(props: PageName) {
+  const pageName = props.name
+  return (
+    <NextLink href={"/"+ pageName} passHref>
+      <Link pb={5} as={'button'} color='teal'>
+        <Icon as={ArrowBackIcon} />
+        Back to {pageName} list
+      </Link>
+    </NextLink>
+  ) 
+
+}
