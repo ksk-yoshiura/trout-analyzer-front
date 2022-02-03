7894f1f5ce28e9bd656c9f9ac3b55f18a7c77dd4
keisuke <mrtambourineman8358@gmail.com>
2022-02-03 22:34:55 +0900

    thumb image

---
+++ src/components/shared/ThumbImage.tsx
@@ -0,0 +1,46 @@
+import {
+  useState,
+  useEffect,
+  FC
+} from "react"
+
+
+type ThumbProps = {
+  file: File | null;
+};
+
+const Thumb: FC<ThumbProps> = ({ file }) => {
+  const [loading, setLoading] = useState<Boolean>(true);
+  const [thumb, setThumb] = useState<string>();
+
+  useEffect(() => {
+    const reader = new FileReader();
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setThumb(reader.result as string);
+      };
+      setLoading(false);
+    }
+  }, [file]);
+
+  if (!file) {
+    return null;
+  }
+
+  if (loading) {
+    return <p>Loading....</p>;
+  }
+
+  return (
+    <img
+      src={thumb}
+      alt={file.name}
+      className="img-thumbnail mt-2"
+      height={200}
+      width={200}
+    />
+  );
+};
+
+export default Thumb 
