58683a13c7221f9901249aa624d5ab1df57729ff
keisuke <mrtambourineman8358@gmail.com>
2022-02-05 23:55:52 +0900

    tackle list

--- src/components/model/records/serial_register_partial/SerialRegisterTackleSelect.tsx
+++ src/components/model/records/serial_register_partial/SerialRegisterTackleSelect.tsx
@@ -1,14 +1,21 @@
 import {
   Select
 } from "@chakra-ui/react";
-import { TackleListMock } from '../../mock/tackles/tackle_list_serail_register_mock'
+import useSWR from 'swr'
+import { TacklesApiResponse } from "../../../../pages/api/tackles/index"
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 export default function TackleSelect(field: any) {
+  // APIからデータ取得
+  const { data, error } = useSWR<TacklesApiResponse, Error>('/api/tackles/', fetcher)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <p>Loading...</p>
 
   return (
     <Select {...field} w='100wh' placeholder='Name'>
       {
-        TackleListMock.map((item, index) => {
+        data.tackles?.map((item, index) => {
           return (
             <option key={index} value={item.id}>
               {item.rod}:{item.reel}:{item.line}
