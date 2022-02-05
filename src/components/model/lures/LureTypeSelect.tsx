d5e7ea55bc4e7b52ea41b205fee5a4b654be82f2
keisuke <mrtambourineman8358@gmail.com>
2022-02-05 22:53:37 +0900

    lure tyeps data from api

--- src/components/model/lures/LureTypeSelect.tsx
+++ src/components/model/lures/LureTypeSelect.tsx
@@ -1,16 +1,24 @@
 import {
   Select,
 } from '@chakra-ui/react'
-import { LureTypeSelectMock } from '../mock/lures/lure_type_select_mock'
+import useSWR from 'swr'
+import { LureTypesApiResponse } from "../../../pages/api/lure_types/index"
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 export default function LureTypeSelect() {
+  // APIからデータ取得
+  const { data, error } = useSWR<LureTypesApiResponse, Error>('/api/lure_types/', fetcher)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <p>Loading...</p>
+
   return (
     <Select w={150} placeholder='Lure Type'>
       {
-        LureTypeSelectMock.map((item, index) => {
+        data.lure_types?.map((item, index) => {
           return (
             <option key={index} value={item.id}>
-              {item.type}
+              {item.type_name}
             </option>
           )
         })
