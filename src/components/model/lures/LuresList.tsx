458cfc52c63142a179d2b4c792285c0952ddc9a8
keisuke <mrtambourineman8358@gmail.com>
2022-02-05 22:59:38 +0900

    lure list data from api

--- src/components/model/lures/LuresList.tsx
+++ src/components/model/lures/LuresList.tsx
@@ -18,10 +18,19 @@ import {
 import NextLink from "next/link"
 import { LureListMock } from '../mock/lures/lure_list_mock'
 import LureDetail from './LureDetail'
+import useSWR from 'swr'
+import { LuresApiResponse } from "../../../pages/api/lures/index"
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 export default function LuresList(): JSX.Element {
+  // モーダル
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [chosenId, idState] = useState(0)
+  // APIからデータ取得
+  const { data, error } = useSWR<LuresApiResponse, Error>('/api/lures/', fetcher)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <p>Loading...</p>
 
   function clickHandler(value: string) {
     // 型変換
@@ -57,7 +66,7 @@ export default function LuresList(): JSX.Element {
     <>
       <Wrap spacing={5}>
         {
-          LureListMock.map((item, index) => {
+          data.lures?.map((item, index) => {
             return (
               <WrapItem key={index} onClick={() => { onOpen(), clickHandler(item.id) }} as={"button"}>
                 <Box w={160} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
