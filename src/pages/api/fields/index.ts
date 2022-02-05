66609f9f79a59631e140cff092378c997f22159b
keisuke <mrtambourineman8358@gmail.com>
2022-02-05 21:43:38 +0900

    field index api

--- src/pages/api/fields/index.ts
+++ src/pages/api/fields/index.ts
@@ -1,155 +1,48 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export type Line = {
+export type Field = {
   id: string
   imageUrl: string
   imageAlt: string
   createdAt: string
-  lineType: string
+  lastVisitedAt: string
   name: string
-  thickness: string
+  address: string
+  frequency: string
 }
 
 // API のレスポンス型
-export type LinesApiResponse = {
-  line?: Line[]
+export type FieldsApiResponse = {
+  field?: Field[]
   debugMessage?: string
 }
 
 // API のエントリポイント
-export default function LinesApi(
+export default function FieldsApi(
   req: NextApiRequest,
-  res: NextApiResponse<LinesApiResponse>
+  res: NextApiResponse<FieldsApiResponse>
 ): void {
-  const id = req.query.id as string
-  const line = fetchLineData()
-  if (line) {
-    res.status(200).json({ line })
+  const field = fetchFieldData()
+  if (field) {
+    res.status(200).json({ field })
   } else {
-    res.status(400).json({ debugMessage: `Line [id=${id}] not found` })
+    res.status(400).json({ debugMessage: `Field not found` })
   }
 }
 
 // 擬似的なデータフェッチ関数
-function fetchLineData(): Line[] | undefined {
-  const lines: Line[] = [
+function fetchFieldData(): Field[] | undefined {
+  const fields: Field[] = [
     {
       id: '1',
       imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
+      imageAlt: 'lure image',
       createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
+      lastVisitedAt: '2021/12/28',
+      name: 'super good fishing park',
+      address: 'shiga prefecture',
+      frequency: '5'
     },
-    {
-      id: '2',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '3',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '4',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '5',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '6',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '7',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '8',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '9',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '10',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '11',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '12',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    },
-    {
-      id: '13',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'line image',
-      createdAt: '2021/12/28',
-      lineType: 'nylon',
-      name: 'super good nylon',
-      thickness: '3'
-    }
   ]
-  return lines
+  return fields
 }
