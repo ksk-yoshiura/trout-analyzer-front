056e6fbdd5035691b02666b3d0fd26fb1d2d6689
keisuke <mrtambourineman8358@gmail.com>
2022-02-06 23:53:11 +0900

    fields select

--- src/components/model/preparation/PreparationFieldSelect.tsx
+++ src/components/model/preparation/PreparationFieldSelect.tsx
@@ -1,13 +1,21 @@
 import {
   Select,
 } from '@chakra-ui/react'
-import { FieldSelectMock } from '../mock/fields/field_select_mock'
+import useSWR from 'swr'
+import { FieldsApiResponse } from "../../../pages/api/fields/index"
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 export default function LureTypeSelect() {
+  // APIからデータ取得
+  const { data, error } = useSWR<FieldsApiResponse, Error>('/api/fields/', fetcher)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <p>Loading...</p>
+
   return (
     <Select mr={5} w="100%" placeholder='Select Field'>
       {
-        FieldSelectMock.map((item, index) => {
+        data.fields?.map((item, index) => {
           return (
             <option key={index} value={item.id}>
               {item.name}
