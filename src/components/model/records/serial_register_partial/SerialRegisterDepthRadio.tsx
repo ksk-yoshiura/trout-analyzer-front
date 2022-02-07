6c446a0bb8de6f1923c6388750a8bd7fd27fbd7a
keisuke <mrtambourineman8358@gmail.com>
2022-02-08 01:15:00 +0900

    depth data from api

--- src/components/model/records/serial_register_partial/SerialRegisterDepthRadio.tsx
+++ src/components/model/records/serial_register_partial/SerialRegisterDepthRadio.tsx
@@ -6,6 +6,12 @@ import {
 import RadioCard from '../../../shared/RadioCard';
 import { DepthRadiotOptionMock } from '../../mock/serial_register/serial_register_radio_mock'
 
+import useSWR from 'swr'
+import { PatternConditionsApiResponse } from "../../../../pages/api/pattern_conditions/type_num/[type_num]"
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
+const depthTypeNum = 3;
+
 export default function DepthRadio() {
 
   const { getRadioProps } = useRadioGroup({
@@ -14,10 +20,19 @@ export default function DepthRadio() {
     onChange: console.log // TODO：valueをセットする関数を用意
   })
 
+  // APIからデータ取得
+  const { data, error } = useSWR<PatternConditionsApiResponse, Error>('/api/pattern_conditions/type_num/' + depthTypeNum, fetcher)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <p>Loading...</p>
+
+  // 深度の名称配列作成
+  const depthData = data.pattern_condition?.map(function (value) {
+    return value.type_name
+  })
 
   return (
     <Wrap>
-      {DepthRadiotOptionMock.map((value) => {
+      {depthData?.map((value) => {
         const radio = getRadioProps({ value })
         return (
           <WrapItem key={value}>
