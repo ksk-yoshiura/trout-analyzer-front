52c083323ef36bd917d24a86bdcf23b7f10f9eeb
keisuke <mrtambourineman8358@gmail.com>
2022-02-08 01:16:39 +0900

    removed mock

--- src/components/model/mock/serial_register/serial_register_radio_mock.ts
+++
@@ -1,3 +0,0 @@
-export const ResultRadiotOptionMock = [ 'caught', 'hit', 'chased']
-export const SpeedRadiotOptionMock = [ 'fast', 'mormal', 'slow', 'super-slow']
-export const DepthRadiotOptionMock = [ 'top', 'shallow', 'medium', 'deep', 'bottom']
