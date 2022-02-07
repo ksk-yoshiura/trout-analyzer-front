965dddb91502926b449ea6ad7c4af0577177144a
keisuke <mrtambourineman8358@gmail.com>
2022-02-07 22:53:25 +0900

    pattern detail mock

--- src/components/model/records/RecordPatternDetail.tsx
+++ src/components/model/records/RecordPatternDetail.tsx
@@ -2,71 +2,77 @@ import {
   Box,
   Image
 } from '@chakra-ui/react'
-import { RecordDetailMock } from '../mock/records/record_detail_mock'
 import RecordPatternBadgeDetail from './pattern_detail_partial/RecordPatternBadgeDetail'
 import RecordPatternLureDetail from './pattern_detail_partial/RecordPatternLureDetail'
 import RecordPatternReelDetail from './pattern_detail_partial/RecordPatternReelDetail'
 import RecordPatternRodDetail from './pattern_detail_partial/RecordPatternRodDetail'
 import RecordPatternLineDetail from './pattern_detail_partial/RecordPatternLineDetail'
 
+import useSWR from 'swr'
+import { PatternApiResponse } from "../../../pages/api/patterns/[id]"
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
+
 type DetailProps = {
   chosenId: number
 }
 
 export default function RecordPatternDetail(props: DetailProps): JSX.Element {
-  const property = RecordDetailMock
-  // TODO：IDを取り出して詳細を取得
-  console.log(props)
-
-  const { result, weather, depth, speed } = property.badge
-  const { lureType, lureName, lureCompany, lureColor, lureWeight } = property.lure
-  const { reelType, reelName, reelCompany, reelGear } = property.reel
-  const { rodName, rodHardness, rodLength, rodCompany } = property.rod
-  const { lineName, lineThickness, lineType, lineCompany } = property.line
-
+  // ID取得
+  const { chosenId } = props
+  // APIからデータ取得
+  const { data, error } = useSWR<PatternApiResponse, Error>('/api/patterns/' + chosenId, fetcher)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <p>Loading...</p>
 
   return (
     <Box maxW='sm' overflow='hidden'>
-      <Image src={property.imageUrl} alt={property.imageAlt} borderRadius='lg' />
+      <Image src={data.pattern?.imageUrl} alt={data.pattern?.imageAlt} borderRadius='lg' />
 
-      <Box p='2'>
-        <RecordPatternBadgeDetail
-          lureType={lureType}
-          result={result}
-          weather={weather}
-          depth={depth}
-          speed={speed}
-        />
+      {
+        data.pattern ?
+          <Box p='2'>
+            <RecordPatternBadgeDetail
+              lureType={data.pattern.lure.lureType}
+              result={data.pattern.badge.result}
+              weather={data.pattern.badge.weather}
+              depth={data.pattern.badge.depth}
+              speed={data.pattern.badge.speed}
+            />
 
-        <RecordPatternLureDetail
-          lureType={lureType}
-          lureName={lureName}
-          lureCompany={lureCompany}
-          lureColor={lureColor}
-          lureWeight={lureWeight}
-        />
+            <RecordPatternLureDetail
+              lureType={data.pattern.lure.lureType}
+              lureName={data.pattern.lure.lureName}
+              lureCompany={data.pattern.lure.lureCompany}
+              lureColor={data.pattern.lure.lureColor}
+              lureWeight={data.pattern.lure.lureWeight}
+            />
 
-        <RecordPatternReelDetail
-          reelName={reelName}
-          reelType={reelType}
-          reelCompany={reelCompany}
-          reelGear={reelGear}
-        />
+            <RecordPatternReelDetail
+              reelName={data.pattern.reel.reelName}
+              reelType={data.pattern.reel.reelType}
+              reelCompany={data.pattern.reel.reelCompany}
+              reelGear={data.pattern.reel.reelGear}
+            />
 
-        <RecordPatternRodDetail
-          rodName={rodName}
-          rodHardness={rodHardness}
-          rodCompany={rodCompany}
-          rodLength={rodLength}
-        />
+            <RecordPatternRodDetail
+              rodName={data.pattern.rod.rodName}
+              rodHardness={data.pattern.rod.rodHardness}
+              rodCompany={data.pattern.rod.rodCompany}
+              rodLength={data.pattern.rod.rodLength}
+            />
 
-        <RecordPatternLineDetail
-          lineName={lineName}
-          lineThickness={lineThickness}
-          lineCompany={lineCompany}
-          lineType={lineType}
-        />
-      </Box>
+            <RecordPatternLineDetail
+              lineName={data.pattern.line.lineName}
+              lineThickness={data.pattern.line.lineThickness}
+              lineCompany={data.pattern.line.lineCompany}
+              lineType={data.pattern.line.lineType}
+            />
+          </Box>
+
+          : <></>
+      }
+
     </Box>
   )
 }
