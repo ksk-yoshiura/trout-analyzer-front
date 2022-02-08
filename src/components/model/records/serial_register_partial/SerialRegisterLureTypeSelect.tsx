2ffc15b092c4e687da755d2694deaf7d2e6d9a8f
keisuke <mrtambourineman8358@gmail.com>
2022-02-08 23:48:28 +0900

    lure type seperate from select because  useSWR conflicts with hook

---
+++ src/components/model/records/serial_register_partial/SerialRegisterLureTypeSelect.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import {
+  Select
+} from "@chakra-ui/react";
+import LureSelect from './SerialRegisterLureSelect'
+import useSWR from 'swr'
+import { LureTypesApiResponse } from "../../../../pages/api/lure_types/index"
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
+
+export default function LureTypeSelect(field: any) {
+  const [lureTypeId, setLureTypeId] = useState('0')
+
+  // APIからデータ取得
+  // ルアータイプリストデータ
+  const { data, error } = useSWR<LureTypesApiResponse, Error>('/api/lure_types', fetcher)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <p>Loading...</p>
+  
+  function changeHandler(event: any) { // TODO：anyで一旦退避
+    const { target } = event;
+    if (!(target instanceof HTMLSelectElement)) {
+      return; // or throw new TypeError();
+    }
+    const targetLureTypeId = target.value
+    setLureTypeId(targetLureTypeId)
+  }
+
+  return (
+    <>
+      <Select mb={2} w={130} onChange={event => changeHandler(event)} placeholder='Lure Type'>
+        {
+          data.lure_types?.map((item, index) => {
+            return (
+              <option key={index} value={item.id}>
+                {item.type_name}
+              </option>
+            )
+          })
+        }
+      </Select>
+      <LureSelect lureTypeId={lureTypeId} field={field} />
+    </>
+  )
+}
