43f48c49a4e1eeb2dbd76220403943d022349ad5
keisuke <mrtambourineman8358@gmail.com>
2022-02-08 23:47:27 +0900

    lure select renewed

--- src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
+++ src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
@@ -1,52 +1,28 @@
-import { useState, useEffect } from 'react'
 import {
   Select
 } from "@chakra-ui/react";
-import { LureTypeSelectMock } from '../../mock/lures/lure_type_select_mock'
-import { LureNameListMock } from '../../mock/lures/lure_name_list_mock'
-
 import useSWR from 'swr'
-import { LureTypesApiResponse } from "../../../../pages/api/lure_types/index"
-import { LuresApiResponse } from "../../../../pages/api/lures/[type_num]"
+import { LuresApiResponse } from "../../../../pages/api/lures/type_num/[type_num]"
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
-export default function LureSelect(field: any) {
-  const [lureTypeId, setLureTypeId] = useState('0')
+import { LureNameListMock } from '../../mock/lures/lure_name_list_mock'
+type LureTypeProps = {
+  lureTypeId: string
+  field: any // TODO : any回避
+}
 
-  function changeHandler(event: any) { // TODO：anyで一旦退避
-    const { target } = event;
-    if (!(target instanceof HTMLSelectElement)) {
-      return; // or throw new TypeError();
-    }
-    const targetLureTypeId = target.value
-    setLureTypeId(targetLureTypeId)
-  }
+export default function LureSelect(props: LureTypeProps) {
+  const { lureTypeId, field } = props
 
-  // APIからデータ取得
-  // ルアータイプリスト
-  const { data, error } = useSWR<LureTypesApiResponse, Error>('/api/lure_types/', fetcher)
+  // ルアーデータリスト
+  const { data, error } = useSWR<LuresApiResponse, Error>('/api/lures/type_num/' + lureTypeId, fetcher)
   if (error) return <p>Error: {error.message}</p>
-  if (!data) return <p>Loading...</p>
-
   return (
-    <>
-      <Select mb={2} w={130} onChange={event => changeHandler(event)} placeholder='Lure Type'>
-        {
-          data.lure_types?.map((item, index) => {
-            return (
-              <option key={index} value={item.id}>
-                {item.type_name}
-              </option>
-            )
-          })
-        }
-      </Select>
-      {
-        lureTypeId !== '0' && lureTypeId ?
-          <Select w='100wh' {...field} onChange={(event) => changeHandler(event)} placeholder='Name'>
+    lureTypeId !== '0' && lureTypeId ?
+          <Select w='100wh' {...field} placeholder='Select lure'>
             {
-              LureNameListMock.map((item, index) => {
+              data?.lures?.map((item, index) => {
                 return (
                   <option key={index} value={item.id}>
                     {item.name} :{item.color}
@@ -55,7 +31,5 @@ export default function LureSelect(field: any) {
               })
             }
           </Select> : <></>
-      }
-    </>
   )
 }
