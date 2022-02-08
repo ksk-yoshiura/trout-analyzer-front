cac72b2f52e52fd39c03f0f5d3635f536f7e26e0
keisuke <mrtambourineman8358@gmail.com>
2022-02-08 23:49:55 +0900

    removed mock

--- src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
+++ src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
@@ -6,7 +6,6 @@ import { LuresApiResponse } from "../../../../pages/api/lures/type_num/[type_num
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
-import { LureNameListMock } from '../../mock/lures/lure_name_list_mock'
 type LureTypeProps = {
   lureTypeId: string
   field: any // TODO : any回避
@@ -20,16 +19,16 @@ export default function LureSelect(props: LureTypeProps) {
   if (error) return <p>Error: {error.message}</p>
   return (
     lureTypeId !== '0' && lureTypeId ?
-          <Select w='100wh' {...field} placeholder='Select lure'>
-            {
-              data?.lures?.map((item, index) => {
-                return (
-                  <option key={index} value={item.id}>
-                    {item.name} :{item.color}
-                  </option>
-                )
-              })
-            }
-          </Select> : <></>
+      <Select w='100wh' {...field} placeholder='Select lure'>
+        {
+          data?.lures?.map((item, index) => {
+            return (
+              <option key={index} value={item.id}>
+                {item.name} :{item.color}
+              </option>
+            )
+          })
+        }
+      </Select> : <></>
   )
 }
