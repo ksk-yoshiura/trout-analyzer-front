85eed323f5264d685c4b7b0c6304894ea4d41c0a
keisuke <mrtambourineman8358@gmail.com>
2022-02-09 22:12:16 +0900

    replaced by axios

--- src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
+++ src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
@@ -3,8 +3,12 @@ import {
 } from "@chakra-ui/react";
 import useSWR from 'swr'
 import { LuresApiResponse } from "../../../../pages/api/lures/type_num/[type_num]"
+import axios from'axios'
 
-const fetcher = (url: string) => fetch(url).then((r) => r.json())
+const fetcher = (url: string) => axios(url)
+.then((res) => {
+  return res.data
+})
 
 type LureTypeProps = {
   lureTypeId: string
@@ -14,12 +18,14 @@ type LureTypeProps = {
 export default function LureSelect(props: LureTypeProps) {
   const { lureTypeId, field } = props
 
+  console.log(field)
+
   // ルアーデータリスト
   const { data, error } = useSWR<LuresApiResponse, Error>('/api/lures/type_num/' + lureTypeId, fetcher)
   if (error) return <p>Error: {error.message}</p>
   return (
     lureTypeId !== '0' && lureTypeId ?
-      <Select w='100wh' {...field} placeholder='Select lure'>
+      <Select {...field} w='100wh' placeholder='Select lure' onChange={(e)=> e.target.value  } >
         {
           data?.lures?.map((item, index) => {
             return (
