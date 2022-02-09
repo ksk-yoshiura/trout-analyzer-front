4e5e2130fc74f59d5b7b564a6c4f0f54261c8cad
keisuke <mrtambourineman8358@gmail.com>
2022-02-09 22:13:14 +0900

    replaced by axios

--- src/components/model/reels/ReelDetail.tsx
+++ src/components/model/reels/ReelDetail.tsx
@@ -6,8 +6,12 @@ import {
 } from '@chakra-ui/react'
 import useSWR from 'swr'
 import { ReelsApiResponse } from "../../../pages/api/reels/[id]"
+import axios from'axios'
 
-const fetcher = (url: string) => fetch(url).then((r) => r.json())
+const fetcher = (url: string) => axios(url)
+.then((res) => {
+  return res.data
+})
 
 type DetailProps = {
   chosenId: number
