7e926c520a8c9f6b548018975679d9ee9284a231
keisuke <mrtambourineman8358@gmail.com>
2022-02-09 22:13:34 +0900

    replaced by axios

--- src/components/model/rods/RodDetail.tsx
+++ src/components/model/rods/RodDetail.tsx
@@ -6,8 +6,12 @@ import {
 } from '@chakra-ui/react'
 import useSWR from 'swr'
 import { RodsApiResponse } from "../../../pages/api/rods/[id]"
+import axios from'axios'
 
-const fetcher = (url: string) => fetch(url).then((r) => r.json())
+const fetcher = (url: string) => axios(url)
+.then((res) => {
+  return res.data
+})
 
 type DetailProps = {
   chosenId: number
