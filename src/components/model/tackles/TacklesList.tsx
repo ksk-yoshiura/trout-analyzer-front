d57faf16f5fab6d793019029d65992c0cd0c4a4e
keisuke <mrtambourineman8358@gmail.com>
2022-02-10 21:42:49 +0900

    tackle list data

--- src/components/model/tackles/TacklesList.tsx
+++ src/components/model/tackles/TacklesList.tsx
@@ -19,9 +19,23 @@ import NextLink from "next/link"
 import { RodListMock } from '../mock/rods/rod_list_mock'
 import RodDetail from './TackleDetail'
 
+import useSWR from 'swr'
+import { TacklesApiResponse } from "../../../pages/api/tackles/index"
+import axios from'axios'
+
+const fetcher = (url: string) => axios(url)
+.then((res) => {
+  return res.data
+})
+
+
 export default function TacklesList(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [chosenId, idState] = useState(0)
+  // APIからデータ取得
+  const { data, error } = useSWR<TacklesApiResponse, Error>('/api/tackles/', fetcher)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <p>Loading...</p>
 
   function clickHandler(value: string) {
     // 型変換
@@ -52,57 +66,56 @@ export default function TacklesList(): JSX.Element {
     )
   }
 
-
   return (
     <>
       <Wrap spacing={5}>
         {
-          RodListMock.map((item, index) => {
+          data.tackles?.map((item, index) => {
             return (
               <WrapItem key={index} onClick={() => { onOpen(), clickHandler(item.id) }} as={"button"}>
-                <Box w={100} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                  <Image src={item.imageUrl} alt={item.imageAlt} />
-
+              <Box w={450} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' display='flex'>
+                <Box w={150} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                  <Image src={item.rod.imageUrl} alt={item.rod.imageAlt} />
                   <Box p='2'>
-                    <Box display='flex' alignItems='baseline'>
-                      <Badge borderRadius='full' px='2' mr={1} colorScheme='teal'>
-                        New
-                      </Badge>
-                      <Badge borderRadius='full' px='2' color='gray.500'>
-                        {item.hardness}
-                      </Badge>
+                    <Box
+                      mt='1'
+                      fontWeight='semibold'
+                      as='h3'
+                      lineHeight='tight'
+                    >
+                      {item.rod.name}
                     </Box>
+
+                  </Box>
+                </Box>
+                <Box w={150} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                  <Image src={item.reel.imageUrl} alt={item.reel.imageAlt} />
+                  <Box p='2'>
                     <Box
                       mt='1'
                       fontWeight='semibold'
                       as='h4'
                       lineHeight='tight'
-                      isTruncated
                     >
-                      {item.name}
-                    </Box>
-                    <Box
-                      color='gray.500'
-                      fontWeight='semibold'
-                      letterSpacing='wide'
-                      fontSize='xs'
-                      textTransform='uppercase'
-                      ml='2'
-                    >
-                      length {item.length} ft
-                    </Box>
-                    <Box
-                      color='gray.500'
-                      fontWeight='semibold'
-                      letterSpacing='wide'
-                      fontSize='xs'
-                      textTransform='uppercase'
-                      ml='2'
-                    >
-                      added {item.createdAt}
+                      {item.reel.name}
                     </Box>
 
                   </Box>
+                </Box>
+                <Box w={150} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                  <Image src={item.line.imageUrl} alt={item.line.imageAlt} />
+                  <Box p='2'>
+                    <Box
+                      mt='1'
+                      fontWeight='semibold'
+                      as='h4'
+                      lineHeight='tight'
+                    >
+                      {item.line.name}
+                    </Box>
+
+                  </Box>
+                </Box>
                 </Box>
               </WrapItem>
             )
