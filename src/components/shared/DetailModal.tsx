624e4f57e23dca5207fceaeb653d52e76cd4b7e5
keisuke <mrtambourineman8358@gmail.com>
2022-02-12 17:40:21 +0900

    delete modal

--- src/components/shared/DetailModal.tsx
+++ src/components/shared/DetailModal.tsx
@@ -7,6 +7,8 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  useDisclosure,
+  Modal,
 } from '@chakra-ui/react'
 import NextLink from "next/link"
 
@@ -14,32 +16,55 @@ type DetailProps = {
   chosenId?: number
   title: string
   children?: ReactNode;
-} 
+}
 
 export default function DetailModal(props: DetailProps) {
+  // 削除モーダル
+  const { isOpen, onOpen, onClose } = useDisclosure()
   // 各値
   const { chosenId, title, children } = props
   // 編集ページリンク
   const editURL = '/' + title + 's/edit/' + chosenId
-  // モーダルタイトル
-  const modalTitle = title 
+  // 詳細モーダルタイトル
+  const modalTitle = title
 
-  return (
-    <>
-    <ModalOverlay />
-        <ModalContent>
-          <ModalHeader textTransform={'capitalize'}>{modalTitle + ' Detail'}</ModalHeader>
+  // 削除モーダル
+  const DeletelModal = () => {
+    return (
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent h={125} margin={'auto'}>
           <ModalCloseButton />
-          <ModalBody>
-            { children }
+          <ModalHeader></ModalHeader>
+          <ModalBody display={'flex'} justifyContent={'space-around'}>
+            <Button colorScheme='gray' variant='solid'>Cancel</Button>
+            <Button colorScheme='red' variant='solid'>Confirm Delete</Button>
           </ModalBody>
-
           <ModalFooter>
-            <NextLink href={ editURL }  passHref>
-              <Button variant='ghost'>Edit</Button>
-            </NextLink>
           </ModalFooter>
         </ModalContent>
+      </Modal>
+    )
+  }
+
+  return ( // 詳細モーダル
+    <>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader textTransform={'capitalize'}>{modalTitle + ' Detail'}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          {children}
+        </ModalBody>
+
+        <ModalFooter display={'flex'} justifyContent={'space-between'}>
+          <Button colorScheme='red' variant='solid' onClick={() => onOpen()}>Delete</Button>
+          <NextLink href={editURL} passHref>
+            <Button colorScheme='blue' variant='solid'>Edit</Button>
+          </NextLink>
+        </ModalFooter>
+      </ModalContent>
+      <DeletelModal />
     </>
   )
 }
