017e11c027f93489849aab258a64299e5921f4c5
keisuke <mrtambourineman8358@gmail.com>
2022-02-14 23:11:24 +0900

    record badge detail jest

---
+++ src/__tests__/components/models/records/pattern_detail_partial/RecordPatternBadgeDetail.spec.tsx
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from '../../../../utils';
+import RecordPatternBadgeDetail from '../../../../../components/model/records/pattern_detail_partial/RecordPatternBadgeDetail';
+
+test('RecordPatternBadgeDetail', () => {
+  // バッヂ値
+  const badgeProps = { 
+    'result': 'caught',
+    'lureType': 'spoon',
+    'weather': 'sunny',
+    'depth': 'bottom',
+    'speed': 'fast',
+  }
+  const { asFragment } = render(<RecordPatternBadgeDetail 
+    lureType={badgeProps.result} 
+    result={badgeProps.lureType} 
+    weather={badgeProps.weather}
+    depth={badgeProps.depth}
+    speed={badgeProps.speed}
+  />);
+  expect(asFragment()).toMatchSnapshot();
+});
