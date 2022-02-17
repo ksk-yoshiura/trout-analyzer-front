cd3e177724dba5cb94d4a8dbe91559e73d033810
keisuke <mrtambourineman8358@gmail.com>
2022-02-17 23:40:03 +0900

    axiost post and put in line

--- src/components/model/lines/LineForm.tsx
+++ src/components/model/lines/LineForm.tsx
@@ -57,18 +57,55 @@ export default function LineForm() {
   if (error) return <p>Error: {error.message}</p>
   if (!data) return <p>Loading...</p>
 
+  // API登録・更新
   function handleSendLineData(values: LineData) {
-    alert(JSON.stringify(values))
-    // リストページに遷移
-    router.push('/lines')
-    // アラート代わりにトーストを使用
-    toast({
-      title: 'Line registered!',
-      description: "We've created your line data for you.",
-      status: 'success',
-      duration: 9000,
-      isClosable: true,
-    })
+    if (id) { // ラインIDがある場合は更新
+      axios.put('/api/lines/edit/' + id, values)
+        .then(function () {
+          // リストページに遷移
+          router.push('/lines')
+          // アラート代わりにトーストを使用
+          toast({
+            title: 'Line updated!',
+            description: "We've updated your line data for you.",
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+        })
+        .catch(function (error) {
+          toast({
+            title: 'Failed!',
+            description: error,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+          })
+        })
+    } else { // ラインIDがない場合は登録
+      axios.post('/api/lines/create', values)
+        .then(function () {
+          // リストページに遷移
+          router.push('/lines')
+          // アラート代わりにトーストを使用
+          toast({
+            title: 'Line registered!',
+            description: "We've created your line data for you.",
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+        })
+        .catch(function (error) {
+          toast({
+            title: 'Failed!',
+            description: error,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+          })
+        })
+    }
   }
 
   function validateData(value: LineData) {
