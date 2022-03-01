63cec77ee3c16765c2dab8154dd968d5cad841c0
keisuke <mrtambourineman8358@gmail.com>
2022-03-01 23:18:07 +0900

    sign up page

--- src/components/page/auth/sign_up.tsx
+++ src/components/page/auth/sign_up.tsx
@@ -1,7 +1,21 @@
 import SignUpForm from '../../model/auth/SignUpForm'
+import {
+  Link,
+  Icon
+} from '@chakra-ui/react'
+import { ArrowBackIcon } from '@chakra-ui/icons'
+import NextLink from "next/link"
 
 export default function SignUpPage(): JSX.Element {
   return (
-    <SignUpForm />
+    <>
+      <SignUpForm />
+      <NextLink href="/login" passHref>
+        <Link pb={5} as={'button'} color='teal'>
+          <Icon as={ArrowBackIcon} />
+          login
+        </Link>
+      </NextLink>
+    </>
   );
 }
