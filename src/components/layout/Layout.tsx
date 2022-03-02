e7381585d068db0687f68d89337b0464ed9a054e
keisuke <mrtambourineman8358@gmail.com>
2022-03-02 23:17:35 +0900

    layout change when login

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,26 +1,44 @@
 import React, { ReactNode } from "react";
-import { 
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router";
+import {
   Flex, Box
 } from "@chakra-ui/react";
 import Header from "./Header";
 import MainContent from "./MainContent";
 import SideMenu from "./SideMenu";
+import Loading from '../shared/Loading'
 
 type Props = {
   children?: ReactNode;
 };
 
 export default function Layout({ children }: Props) {
-  // TODO：ここでレスポンシブ調整
+  // TODO：要解消
+  // Warning: Expected server HTML to contain a matching <div> in <div>.
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  const isReady = router.isReady;
+  
+  if (!isReady) {
+    return <Loading />
+  }
+
   return (
-    <Box>
-      <Header />
-      <Flex>
-        <SideMenu />
-        <MainContent>
-          { children }
-        </MainContent>
-      </Flex>
-    </Box>
+    session ? // ログイン状態か
+      <Box>
+        <Header />
+        <Flex>
+          <SideMenu />
+          <MainContent>
+            {children}
+          </MainContent>
+        </Flex>
+      </Box>
+      : 
+      <MainContent>
+        {children}
+      </MainContent>
   );
 }
