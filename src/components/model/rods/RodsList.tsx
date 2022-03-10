40e3c49e89d65b339fb172b3f5920287ecaf9015
keisuke <mrtambourineman8358@gmail.com>
2022-03-10 23:46:26 +0900

    get rod data api

--- src/components/model/rods/RodsList.tsx
+++ src/components/model/rods/RodsList.tsx
@@ -24,7 +24,7 @@ export default function RodsList(props: FieldProp): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [chosenId, idState] = useState(0)
   // APIからデータ取得
-  const { data, error } = useSWR<RodsApiResponse, Error>('/api/rods/')
+  const { data, error } = useSWR<RodsApiResponse, Error>('rods')
   if (error) return <p>Error: {error.message}</p>
   if (!data) return <Loading />
 
@@ -52,7 +52,7 @@ export default function RodsList(props: FieldProp): JSX.Element {
     <>
       <Wrap spacing={5}>
         {
-          data.rods?.map((item, index) => {
+          data.result?.map((item, index) => {
             return (
               <WrapItem key={index} onClick={() => { onOpen(), clickHandler(item.id) }} type='button' as={"button"}>
                 <Box w={160} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
