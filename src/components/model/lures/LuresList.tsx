07f101c32613e883d7c80816309a03196202e9e6
keisuke <mrtambourineman8358@gmail.com>
2022-03-10 23:49:05 +0900

    api response type in lure

--- src/components/model/lures/LuresList.tsx
+++ src/components/model/lures/LuresList.tsx
@@ -20,7 +20,7 @@ export default function LuresList(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [chosenId, idState] = useState(0)
   // APIからデータ取得
-  const { data, error } = useSWR<LuresApiResponse, Error>('/api/lures/')
+  const { data, error } = useSWR<LuresApiResponse, Error>('lures')
   if (error) return <p>Error: {error.message}</p>
   if (!data) return <Loading />
 
@@ -49,7 +49,7 @@ export default function LuresList(): JSX.Element {
     <>
       <Wrap spacing={5}>
         {
-          data.lures?.map((item, index) => {
+          data.result?.map((item, index) => {
             return (
               <WrapItem key={index} onClick={() => { onOpen(), clickHandler(item.id) }} type='button' as={"button"}>
                 <Box w={160} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
