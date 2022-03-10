ceeee49b30ea511034a245ce759f315446dd6e81
keisuke <mrtambourineman8358@gmail.com>
2022-03-10 23:50:56 +0900

    get reel data api

--- src/components/model/reels/ReelsList.tsx
+++ src/components/model/reels/ReelsList.tsx
@@ -19,7 +19,7 @@ export default function ReelsList(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [chosenId, idState] = useState(0)
   // APIからデータ取得
-  const { data, error } = useSWR<ReelsApiResponse, Error>('/api/reels/')
+  const { data, error } = useSWR<ReelsApiResponse, Error>('reels')
   if (error) return <p>Error: {error.message}</p>
   if (!data) return <Loading />
 
@@ -47,7 +47,7 @@ export default function ReelsList(): JSX.Element {
     <>
       <Wrap spacing={5}>
         {
-          data.reels?.map((item, index) => {
+          data.result?.map((item, index) => {
             return (
               <WrapItem key={index} onClick={() => { onOpen(), clickHandler(item.id) }} type='button' as={"button"}>
                 <Box w={160} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
