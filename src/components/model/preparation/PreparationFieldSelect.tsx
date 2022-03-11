faac467160c31147aa4b255cddf6f399abb6e047
keisuke <mrtambourineman8358@gmail.com>
2022-03-11 23:39:06 +0900

    field list api

--- src/components/model/preparation/PreparationFieldSelect.tsx
+++ src/components/model/preparation/PreparationFieldSelect.tsx
@@ -8,14 +8,14 @@ import { FieldsApiResponse } from "../../../pages/api/fields/index"
 
 export default function FieldSelect() {
   // APIからデータ取得
-  const { data, error } = useSWR<FieldsApiResponse, Error>('/api/fields/')
+  const { data, error } = useSWR<FieldsApiResponse, Error>('fields')
   if (error) return <p>Error: {error.message}</p>
   if (!data) return <Loading />
 
   return (
     <Select mr={5} w="100%" placeholder='Select Field'>
       {
-        data.fields?.map((item, index) => {
+        data.result?.map((item, index) => {
           return (
             <option key={index} value={item.id}>
               {item.name}
