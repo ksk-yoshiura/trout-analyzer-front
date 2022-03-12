bdab76abe848a34efbe964f0f7aa566ea62f4cf2
keisuke <mrtambourineman8358@gmail.com>
2022-03-12 23:14:55 +0900

    replaced by env data and defined default

--- src/pages/api/utils.ts
+++ src/pages/api/utils.ts
@@ -2,6 +2,11 @@
 import { useSession } from "next-auth/react"
 import axios from 'axios'
 
+// デフォルト
+export const axiosDefaultInstance = axios.create({
+  baseURL: process.env.NEXTAUTH_BACK_URL,
+  timeout: 5000
+})
 export const createAxiosInstance = () => { 
   // セッションからアクセストークンを取得
   const { data: session } = useSession();
@@ -9,15 +14,10 @@ export const createAxiosInstance = () => {
   // axiosにヘッダー情報付与
   axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
   axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-  // 基本情報付与
-  const instance = axios.create({
-    baseURL: 'http://localhost:3030/api/',
-    timeout: 5000
-  })
   
   // jwtトークンをアクセストークンから取得
   // ヘッダーに仕込む
-  instance.interceptors.request.use(config => {
+  axiosDefaultInstance.interceptors.request.use(config => {
     if (session?.accessToken && config?.headers) {
       config.headers.Authorization = `Bearer ${session.accessToken}`
     }
@@ -25,6 +25,6 @@ export const createAxiosInstance = () => {
   })
 
   // axiosインスタンス返却
-  return instance
+  return axiosDefaultInstance
 }
 
