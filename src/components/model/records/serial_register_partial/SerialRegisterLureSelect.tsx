4fe617f41de8f9cb9f93738f7814bc2c00a57fc7
keisuke <mrtambourineman8358@gmail.com>
2022-03-13 18:39:05 +0900

    removed fetcher

--- src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
+++ src/components/model/records/serial_register_partial/SerialRegisterLureSelect.tsx
@@ -4,12 +4,6 @@ import {
 } from "@chakra-ui/react";
 import useSWR from 'swr'
 import { LuresApiResponse } from "../../../../pages/api/lures/type_num/[type_num]"
-import axios from'axios'
-
-const fetcher = (url: string) => axios(url)
-.then((res) => {
-  return res.data
-})
 
 type LureTypeProps = {
   lureTypeId: string
@@ -20,7 +14,7 @@ export default function LureSelect(props: LureTypeProps) {
   const { lureTypeId, field } = props
 
   // ルアーデータリスト
-  const { data, error } = useSWR<LuresApiResponse, Error>('lures/type_num/' + lureTypeId, fetcher)
+  const { data, error } = useSWR<LuresApiResponse, Error>('lures/type_num/' + lureTypeId)
   if (error) return <p>Error: {error.message}</p>
   return (
     lureTypeId !== '0' && lureTypeId ?
