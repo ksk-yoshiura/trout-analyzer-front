b2ac76543b08fed02765a180f63128c5042848bd
keisuke <mrtambourineman8358@gmail.com>
2022-03-13 13:45:06 +0900

    tackle api

--- src/components/model/tackles/TacklesList.tsx
+++ src/components/model/tackles/TacklesList.tsx
@@ -17,7 +17,7 @@ export default function TacklesList(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [chosenId, idState] = useState(0)
   // APIからデータ取得
-  const { data, error } = useSWR<TacklesApiResponse, Error>('/api/tackles/')
+  const { data, error } = useSWR<TacklesApiResponse, Error>('tackles')
   if (error) return <p>Error: {error.message}</p>
   if (!data) return <Loading />
 
@@ -45,7 +45,7 @@ export default function TacklesList(): JSX.Element {
     <>
       <Wrap spacing={5}>
         {
-          data.tackles?.map((item, index) => {
+          data.result?.map((item, index) => {
             return (
               <WrapItem key={index} onClick={() => { onOpen(), clickHandler(item.id) }} type='button' as={"button"}>
                 <Box w={450} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' display='flex'>
