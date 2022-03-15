c6f78bf1f28611c24e44822426f62d679d0c8c38
keisuke <mrtambourineman8358@gmail.com>
2022-03-16 01:01:48 +0900

    login not use api in url

--- src/pages/api/utils.ts
+++ src/pages/api/utils.ts
@@ -5,6 +5,12 @@ const apiURL = process.env.NEXT_PUBLIC_BACK_URL
 
 // デフォルト
 export const axiosDefaultInstance = axios.create({
+  baseURL: apiURL,
+  timeout: 5000
+})
+
+// デフォルトAPI
+export const axiosDefaultAPIInstance = axios.create({
   baseURL: apiURL + '/api/',
   timeout: 5000
 })
@@ -14,12 +20,12 @@ export const createAxiosInstance = () => {
   const { data: session } = useSession();
 
   // axiosにヘッダー情報付与
-  axiosDefaultInstance.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-  axiosDefaultInstance.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+  axiosDefaultAPIInstance.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+  axiosDefaultAPIInstance.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
   
   // jwtトークンをアクセストークンから取得
   // ヘッダーに仕込む
-  axiosDefaultInstance.interceptors.request.use(config => {
+  axiosDefaultAPIInstance.interceptors.request.use(config => {
     if (session?.accessToken && config?.headers) {
       config.headers.Authorization = `Bearer ${session.accessToken}`
     }
@@ -27,6 +33,6 @@ export const createAxiosInstance = () => {
   })
 
   // axiosインスタンス返却
-  return axiosDefaultInstance
+  return axiosDefaultAPIInstance
 }
 
