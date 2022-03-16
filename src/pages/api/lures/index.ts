fdd9464c03138d2b8458afdcf05de61a2beee845
keisuke <mrtambourineman8358@gmail.com>
2022-03-16 23:04:34 +0900

    lure list response type

--- src/pages/api/lures/index.ts
+++ src/pages/api/lures/index.ts
@@ -1,11 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type LureType = {
+  ID: string
+  typeName: string
+}
+
 export type Lure = {
   id: string
   imageUrl: string
   imageAlt: string
-  createdAt: string
-  lureType: string
+  CreatedAt: string
+  LureType: LureType
   name: string
 }
 
@@ -30,112 +35,112 @@ export default function LuresApi(
 }
 
 // 擬似的なデータフェッチ関数
-function fetchLureData(): Lure[] | undefined {
-  const lures: Lure[] = [
-    {
-      id: '1',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '2',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '3',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '4',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '5',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '6',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '7',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '8',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '9',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '10',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '11',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '12',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    },
-    {
-      id: '13',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      lureType: 'spoon',
-      name: 'super crank bait'
-    }
-  ]
-  return lures
-}
+// function fetchLureData(): Lure[] | undefined {
+//   const lures: Lure[] = [
+//     {
+//       id: '1',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '2',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '3',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '4',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '5',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '6',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '7',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '8',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '9',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '10',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '11',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '12',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     },
+//     {
+//       id: '13',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       lureType: 'spoon',
+//       name: 'super crank bait'
+//     }
+//   ]
+//   return lures
+// }
