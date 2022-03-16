539fad465562fb9bc4742614b0c95dfb48477ba2
keisuke <mrtambourineman8358@gmail.com>
2022-03-17 00:28:17 +0900

    create vacant data

--- src/components/page/lures/create.tsx
+++ src/components/page/lures/create.tsx
@@ -4,12 +4,45 @@ import {
 import LureForm from '../../model/lures/LureForm'
 import BackToListPageLink from '../../shared/BackToListPageLink'
 
+type LureType = {
+  ID: string
+  typeName: string
+}
+
+type LureData = {
+  ID: string
+  imageUrl: string
+  imageAlt: string
+  CreatedAt: string
+  lastUsedAt: string
+  LureType: LureType
+  name: string
+  companyName: string
+  color: string
+  weight: string
+  frequency: string
+}
+
 export default function LureCreate(): JSX.Element {
+  const chosenId = '0'
+  const data: LureData = { 
+    ID: '',
+    imageUrl: '',
+    imageAlt: '',
+    CreatedAt: '',
+    lastUsedAt: '',
+    LureType: { ID: '', typeName: '' },
+    name: '',
+    companyName: '',
+    color: '',
+    weight: '',
+    frequency: '', 
+  }
   return (
     <>
       <BackToListPageLink name={'lures'} />
       <Flex textAlign="center" w="100wh">
-        <LureForm />
+        <LureForm chosenId={chosenId} data={data} />
       </Flex>
     </>
   )
