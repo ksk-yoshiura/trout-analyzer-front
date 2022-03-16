ebd6dfa6d122a178c8acae797baa63ae3d5415b4
keisuke <mrtambourineman8358@gmail.com>
2022-03-16 22:58:27 +0900

    reel index response type

--- src/pages/api/reels/index.ts
+++ src/pages/api/reels/index.ts
@@ -1,10 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export type Reel = {
-  id: string
+  ID: string
   imageUrl: string
   imageAlt: string
-  createdAt: string
+  CreatedAt: string
   type: string
   gear: string
   name: string
@@ -31,125 +31,125 @@ export default function ReelsApi(
 }
 
 // 擬似的なデータフェッチ関数
-function fetchReelData(): Reel[] | undefined {
-  const reels: Reel[] = [
-    {
-      id: '1',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '2',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '3',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '4',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '5',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '6',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '7',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '8',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '9',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '10',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '11',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '12',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    },
-    {
-      id: '13',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '2021/12/28',
-      type: '3000',
-      gear: 'high',
-      name: 'super good reel'
-    }
-  ]
-  return reels
-}
+// function fetchReelData(): Reel[] | undefined {
+//   const reels: Reel[] = [
+//     {
+//       id: '1',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '2',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '3',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '4',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '5',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '6',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '7',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '8',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '9',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '10',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '11',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '12',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     },
+//     {
+//       id: '13',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '2021/12/28',
+//       type: '3000',
+//       gear: 'high',
+//       name: 'super good reel'
+//     }
+//   ]
+//   return reels
+// }
