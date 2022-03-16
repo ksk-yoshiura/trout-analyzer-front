5e534f51dc6bdc13909ec48fe7c61d39bd9e5714
keisuke <mrtambourineman8358@gmail.com>
2022-03-17 00:27:48 +0900

    edit needs data

--- src/components/page/lures/edit.tsx
+++ src/components/page/lures/edit.tsx
@@ -3,13 +3,26 @@ import {
 } from '@chakra-ui/react'
 import LureForm from '../../model/lures/LureForm'
 import BackToListPageLink from '../../shared/BackToListPageLink'
+import { useRouter } from "next/router";
+import useSWR from 'swr'
+import Loading from '../../shared/Loading'
+import { LuresApiResponse } from "../../../pages/api/lures/[id]"
 
 export default function LureEdit(): JSX.Element {
+
+  // パラメータからルアーID取得
+  const router = useRouter();
+  const { id } = router.query
+
+  // APIからデータ取得
+  const { data, error } = useSWR<LuresApiResponse, Error>('lures/' + id)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <Loading />
   return (
     <>
       <BackToListPageLink name={'lures'} />
       <Flex textAlign="center" w="100wh">
-        <LureForm />
+        <LureForm chosenId={id} data={data.result} />
       </Flex>
     </>
   )
