655e50bb1d21a62eebd610a5acce4d229ef881cf
keisuke <mrtambourineman8358@gmail.com>
2022-03-19 18:43:02 +0900

    rod edit page

--- src/components/page/rods/edit.tsx
+++ src/components/page/rods/edit.tsx
@@ -3,13 +3,25 @@ import {
 } from '@chakra-ui/react'
 import RodForm from '../../model/rods/RodForm'
 import BackToListPageLink from '../../shared/BackToListPageLink'
+import { useRouter } from "next/router";
+import useSWR from 'swr'
+import Loading from '../../shared/Loading'
+import { RodsApiResponse } from "../../../pages/api/rods/[id]"
 
 export default function RodEdit(): JSX.Element {
+  // パラメータからロッドID取得
+  const router = useRouter();
+  const { id } = router.query
+
+  // APIからデータ取得
+  const { data, error } = useSWR<RodsApiResponse, Error>('rods/' + id)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <Loading />
   return (
     <>
       <BackToListPageLink name={'rods'} />
       <Flex textAlign="center" w="100wh">
-        <RodForm />
+        <RodForm chosenId={id} data={data.result} />
       </Flex>
     </>
   )
