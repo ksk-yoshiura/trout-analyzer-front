045fe998cb9a92bd5385239d6dd7210231ae49a5
keisuke <mrtambourineman8358@gmail.com>
2022-03-19 17:56:06 +0900

    reel edit page

--- src/components/page/reels/edit.tsx
+++ src/components/page/reels/edit.tsx
@@ -3,13 +3,25 @@ import {
 } from '@chakra-ui/react'
 import ReelForm from '../../model/reels/ReelForm'
 import BackToListPageLink from '../../shared/BackToListPageLink'
+import { useRouter } from "next/router";
+import useSWR from 'swr'
+import Loading from '../../shared/Loading'
+import { ReelsApiResponse } from "../../../pages/api/reels/[id]"
 
 export default function ReelEdit(): JSX.Element {
+  // パラメータからロッドID取得
+  const router = useRouter();
+  const { id } = router.query
+
+  // APIからデータ取得
+  const { data, error } = useSWR<ReelsApiResponse, Error>('reels/' + id)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <Loading />
   return (
     <>
       <BackToListPageLink name={'reels'} />
       <Flex textAlign="center" w="100wh">
-        <ReelForm />
+        <ReelForm chosenId={id} data={data.result} />
       </Flex>
     </>
   )
