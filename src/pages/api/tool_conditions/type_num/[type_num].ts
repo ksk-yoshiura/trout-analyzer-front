c6afb012212c56a998a14a7604f067001b662ae7
keisuke <mrtambourineman8358@gmail.com>
2022-03-19 16:53:39 +0900

    tool condition response type

--- src/pages/api/tool_conditions/type_num/[type_num].ts
+++ src/pages/api/tool_conditions/type_num/[type_num].ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export type ToolCondition = {
-  id: string
-  type_num: string
-  type_name: string
+  ID: string
+  typeNum: string
+  typeName: string
 }
 
 // API のレスポンス型
@@ -28,78 +28,78 @@ export default function ToolConditionApi(
 }
 
 // 擬似的なデータフェッチ関数
-function fetchToolConditionData(type_num: string): ToolCondition[] | undefined {
-  const tool_conditions: ToolCondition[] = [
-    {
-      id: '1',
-      type_num: '1',
-      type_name: 'UL'
-    },
-    {
-      id: '2',
-      type_num: '1',
-      type_name: 'L'
-    },
-    {
-      id: '3',
-      type_num: '1',
-      type_name: 'ML'
-    },
-    {
-      id: '4',
-      type_num: '1',
-      type_name: 'M'
-    },
-    {
-      id: '5',
-      type_num: '2',
-      type_name: 'normal gear'
-    },
-    {
-      id: '6',
-      type_num: '2',
-      type_name: 'high gear'
-    },
-    {
-      id: '7',
-      type_num: '3',
-      type_name: '1000'
-    },
-    {
-      id: '8',
-      type_num: '3',
-      type_name: '1500'
-    },
-    {
-      id: '9',
-      type_num: '3',
-      type_name: '2000'
-    },
-    {
-      id: '10',
-      type_num: '3',
-      type_name: '2500'
-    },
-    {
-      id: '11',
-      type_num: '4',
-      type_name: 'fluorocarbon'
-    },
-    {
-      id: '12',
-      type_num: '4',
-      type_name: 'nylon'
-    },
-    {
-      id: '13',
-      type_num: '4',
-      type_name: 'PE'
-    },
-    {
-      id: '14',
-      type_num: '4',
-      type_name: 'ester'
-    }
-  ]
-  return tool_conditions.filter(function(value) {return value.type_num === type_num})
-}
+// function fetchToolConditionData(type_num: string): ToolCondition[] | undefined {
+//   const tool_conditions: ToolCondition[] = [
+//     {
+//       id: '1',
+//       type_num: '1',
+//       type_name: 'UL'
+//     },
+//     {
+//       id: '2',
+//       type_num: '1',
+//       type_name: 'L'
+//     },
+//     {
+//       id: '3',
+//       type_num: '1',
+//       type_name: 'ML'
+//     },
+//     {
+//       id: '4',
+//       type_num: '1',
+//       type_name: 'M'
+//     },
+//     {
+//       id: '5',
+//       type_num: '2',
+//       type_name: 'normal gear'
+//     },
+//     {
+//       id: '6',
+//       type_num: '2',
+//       type_name: 'high gear'
+//     },
+//     {
+//       id: '7',
+//       type_num: '3',
+//       type_name: '1000'
+//     },
+//     {
+//       id: '8',
+//       type_num: '3',
+//       type_name: '1500'
+//     },
+//     {
+//       id: '9',
+//       type_num: '3',
+//       type_name: '2000'
+//     },
+//     {
+//       id: '10',
+//       type_num: '3',
+//       type_name: '2500'
+//     },
+//     {
+//       id: '11',
+//       type_num: '4',
+//       type_name: 'fluorocarbon'
+//     },
+//     {
+//       id: '12',
+//       type_num: '4',
+//       type_name: 'nylon'
+//     },
+//     {
+//       id: '13',
+//       type_num: '4',
+//       type_name: 'PE'
+//     },
+//     {
+//       id: '14',
+//       type_num: '4',
+//       type_name: 'ester'
+//     }
+//   ]
+//   return tool_conditions.filter(function(value) {return value.type_num === type_num})
+// }
