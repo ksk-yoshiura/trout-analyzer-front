9012b658ff33335dad5ebb602ad4e16e7468bf54
keisuke <mrtambourineman8358@gmail.com>
2022-03-19 17:56:20 +0900

    reel create page

--- src/components/page/reels/create.tsx
+++ src/components/page/reels/create.tsx
@@ -1,10 +1,25 @@
-import { 
+import {
   Flex
 } from '@chakra-ui/react'
 import ReelForm from '../../model/reels/ReelForm'
 import BackToListPageLink from '../../shared/BackToListPageLink'
 
+type ReelData = {
+  name?: string;
+  company?: string;
+  type?: string;
+  gear?: string;
+  image?: string;
+}
+
 export default function ReelCreate(): JSX.Element {
+  const chosenId = '0'
+  const data: ReelData = {
+    name: '',
+    company: '',
+    type: '',
+    gear: '',
+  }
   return (
     <>
       <BackToListPageLink name={'reels'} />
