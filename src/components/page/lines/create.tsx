a3e756b0543f70fe3edd3641f55dd6f4a7505591
keisuke <mrtambourineman8358@gmail.com>
2022-03-19 16:52:39 +0900

    create line page

--- src/components/page/lines/create.tsx
+++ src/components/page/lines/create.tsx
@@ -4,12 +4,30 @@ import {
 import LineForm from '../../model/lines/LineForm'
 import BackToListPageLink from '../../shared/BackToListPageLink'
 
+type LineData = {
+  imageUrl?: string
+  CreatedAt?: string
+  lineType?: string
+  name?: string
+  company?: string
+  thickness?: number
+}
+
 export default function LineCreate(): JSX.Element {
+  const chosenId = '0'
+  const data: LineData = { 
+    imageUrl: '',
+    CreatedAt: '',
+    lineType: '',
+    name: '',
+    company: '',
+    thickness: 0, 
+  }
   return (
     <>
       <BackToListPageLink name={'lines'} />
       <Flex textAlign="center" w="100wh">
-        <LineForm />
+        <LineForm chosenId={chosenId} data={data} />
       </Flex>
     </>
   )
