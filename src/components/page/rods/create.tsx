1b50eb559b6746c57311c5caa2f75b4757d3b28d
keisuke <mrtambourineman8358@gmail.com>
2022-03-19 18:42:52 +0900

    rod create page

--- src/components/page/rods/create.tsx
+++ src/components/page/rods/create.tsx
@@ -4,12 +4,28 @@ import {
 import RodForm from '../../model/rods/RodForm'
 import BackToListPageLink from '../../shared/BackToListPageLink'
 
+type RodData = {
+  name?: string;
+  company?: string;
+  hardness?: string;
+  length?: string;
+  image?: string;
+}
+
 export default function RodCreate(): JSX.Element {
+  const chosenId = '0'
+  const data: RodData = {
+    name: '',
+    company: '',
+    hardness: '',
+    length: '',
+    image: '',
+  }
   return (
     <>
       <BackToListPageLink name={'rods'} />
       <Flex textAlign="center" w="100wh">
-        <RodForm />
+        <RodForm chosenId={chosenId} data={data} />
       </Flex>
     </>
   )
