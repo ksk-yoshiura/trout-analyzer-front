b2006c0b5b610cc4aa83bf29e534f027e1934c8c
keisuke <mrtambourineman8358@gmail.com>
2022-03-19 23:18:18 +0900

    pattern condition type

--- src/pages/api/pattern_conditions/type_num/[type_num].ts
+++ src/pages/api/pattern_conditions/type_num/[type_num].ts
@@ -2,8 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export type PatternCondition = {
   id: string
-  type_num: string
-  type_name: string
+  typeNum: string
+  typeName: string
 }
 
 // API のレスポンス型
@@ -28,88 +28,88 @@ export default function PatternConditionsApi(
 }
 
 // 擬似的なデータフェッチ関数
-function fetchPatternConditionData(type_num: string): PatternCondition[] | undefined {
-  const pattern_conditions: PatternCondition[] = [
-    {
-      id: '1',
-      type_num: '1',
-      type_name: 'caught'
-    },
-    {
-      id: '2',
-      type_num: '1',
-      type_name: 'bite'
-    },
-    {
-      id: '3',
-      type_num: '1',
-      type_name: 'chased'
-    },
-    {
-      id: '4',
-      type_num: '2',
-      type_name: 'super fast'
-    },
-    {
-      id: '5',
-      type_num: '2',
-      type_name: 'fast'
-    },
-    {
-      id: '6',
-      type_num: '2',
-      type_name: 'normal'
-    },
-    {
-      id: '7',
-      type_num: '2',
-      type_name: 'slow'
-    },
-    {
-      id: '8',
-      type_num: '2',
-      type_name: 'super slow'
-    },
-    {
-      id: '9',
-      type_num: '3',
-      type_name: 'top'
-    },
-    {
-      id: '10',
-      type_num: '3',
-      type_name: 'shallow'
-    },
-    {
-      id: '11',
-      type_num: '3',
-      type_name: 'middle'
-    },
-    {
-      id: '12',
-      type_num: '3',
-      type_name: 'deep'
-    },
-    {
-      id: '13',
-      type_num: '3',
-      type_name: 'bottom'
-    },
-    {
-      id: '14',
-      type_num: '4',
-      type_name: 'sunny'
-    },
-    {
-      id: '15',
-      type_num: '4',
-      type_name: 'rainy'
-    },
-    {
-      id: '16',
-      type_num: '4',
-      type_name: 'cloudy'
-    }
-  ]
-  return pattern_conditions.filter(function(value) {return value.type_num === type_num})
-}
+// function fetchPatternConditionData(type_num: string): PatternCondition[] | undefined {
+//   const pattern_conditions: PatternCondition[] = [
+//     {
+//       id: '1',
+//       type_num: '1',
+//       type_name: 'caught'
+//     },
+//     {
+//       id: '2',
+//       type_num: '1',
+//       type_name: 'bite'
+//     },
+//     {
+//       id: '3',
+//       type_num: '1',
+//       type_name: 'chased'
+//     },
+//     {
+//       id: '4',
+//       type_num: '2',
+//       type_name: 'super fast'
+//     },
+//     {
+//       id: '5',
+//       type_num: '2',
+//       type_name: 'fast'
+//     },
+//     {
+//       id: '6',
+//       type_num: '2',
+//       type_name: 'normal'
+//     },
+//     {
+//       id: '7',
+//       type_num: '2',
+//       type_name: 'slow'
+//     },
+//     {
+//       id: '8',
+//       type_num: '2',
+//       type_name: 'super slow'
+//     },
+//     {
+//       id: '9',
+//       type_num: '3',
+//       type_name: 'top'
+//     },
+//     {
+//       id: '10',
+//       type_num: '3',
+//       type_name: 'shallow'
+//     },
+//     {
+//       id: '11',
+//       type_num: '3',
+//       type_name: 'middle'
+//     },
+//     {
+//       id: '12',
+//       type_num: '3',
+//       type_name: 'deep'
+//     },
+//     {
+//       id: '13',
+//       type_num: '3',
+//       type_name: 'bottom'
+//     },
+//     {
+//       id: '14',
+//       type_num: '4',
+//       type_name: 'sunny'
+//     },
+//     {
+//       id: '15',
+//       type_num: '4',
+//       type_name: 'rainy'
+//     },
+//     {
+//       id: '16',
+//       type_num: '4',
+//       type_name: 'cloudy'
+//     }
+//   ]
+//   return pattern_conditions.filter(function(value) {return value.type_num === type_num})
+// }
