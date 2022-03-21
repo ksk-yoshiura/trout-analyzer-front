e88496fbd2b1308a9f891d52a950b2580dd90770
keisuke <mrtambourineman8358@gmail.com>
2022-03-21 23:59:16 +0900

    select lure type and value used in different component

--- src/components/page/lures/index.tsx
+++ src/components/page/lures/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Flex,
   Button
@@ -8,15 +8,17 @@ import LuresList from '../../model/lures/LuresList'
 import LureTypeSelect from '../../model/lures/LureTypeSelect'
 
 export default function LureIndex(): JSX.Element {
+  // ルアータイプセレクト時タイプIDと値変更hookを別々のコンポーネントで使用する
+  const [typeId, setTypeId] = useState("0")
   return (
     <>
       <Flex pb={5} justifyContent="space-between" w="90%">
-        <LureTypeSelect />
+        <LureTypeSelect setTypeId={setTypeId} />
         <NextLink href="/lures/create" passHref>
           <Button  pl={10} pr={10} colorScheme='teal'>New Lures</Button>
         </NextLink>
       </Flex>
-      <LuresList />
+      <LuresList typeId={typeId} />
     </>
   );
 }
