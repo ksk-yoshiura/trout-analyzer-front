7cf4c1222039d9313edc20ae81b61036bdc382f7
keisuke <mrtambourineman8358@gmail.com>
2022-03-26 20:26:33 +0900

    redirect to login form without login

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,13 +1,24 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
 import { 
   Flex
 } from "@chakra-ui/react";
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 
 type Props = {
   children?: ReactNode;
 };
 
 export default function MainContent({ children }: Props) {
+  const { data: session } = useSession();
+  const router = useRouter();
+  
+  useEffect(() => {
+    // 未ログインの場合、ログインページへリダイレクト
+  if (session !== undefined && router.pathname !== '/login') {
+    router.push('/login')
+  }
+  }, [session])
   return (
     <Flex
       flexDirection="column"
