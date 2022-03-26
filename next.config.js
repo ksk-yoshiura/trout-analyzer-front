cc3ea1dde9b681369222be4eae7fbe42d60d6922
keisuke <mrtambourineman8358@gmail.com>
2022-03-26 21:13:11 +0900

    add next-pwa

--- next.config.js
+++ next.config.js
@@ -2,3 +2,12 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public", // swの出力ディレクトリ
+    // runtimeCaching: []
+  },
+});
