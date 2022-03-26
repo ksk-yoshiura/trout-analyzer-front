faa56963801516136a29bdbdbfa016001cd08cb7
keisuke <mrtambourineman8358@gmail.com>
2022-03-26 20:25:40 +0900

    test with useSession

--- src/__tests__/components/layout/MainContent.spec.tsx
+++ src/__tests__/components/layout/MainContent.spec.tsx
@@ -4,9 +4,20 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { render } from '../../utils';
+import  { useSession } from "next-auth/react";
+import  { Session } from "next-auth";
 import MainContent from '../../../components/layout/MainContent';
 
+jest.mock("next-auth/react")
+
 test('MainContent', () => {
+  const mockSession: Session = {
+    expires: "1",
+    user: { email: "a", name: "Delta" },
+  };
+
+  (useSession as jest.Mock).mockReturnValueOnce([mockSession, false]);
+
   const { asFragment } = render(<MainContent />);
   expect(asFragment()).toMatchSnapshot();
 });
