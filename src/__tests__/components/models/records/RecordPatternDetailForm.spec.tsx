db3bc148981f92bf873f4b1155f1295c8ff3020f
keisuke <mrtambourineman8358@gmail.com>
2022-03-28 00:52:14 +0900

    jest with useRouter in pattern detail form

---
+++ src/__tests__/components/models/records/RecordPatternDetailForm.spec.tsx
@@ -0,0 +1,50 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from '../../../utils';
+import { useSession } from "next-auth/react";
+import { Session } from "next-auth";
+import RecordPatternDetailForm from '../../../../components/model/records/RecordPatternDetailForm';
+
+jest.mock("next-auth/react")
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '/',
+      query: {},
+      asPath: '/',
+      basePath: '/',
+      isLocaleDomain: true,
+      isReady: true,
+      push: jest.fn(),
+      prefetch: jest.fn(),
+      replace: jest.fn(),
+      reload: jest.fn(),
+      back: jest.fn(),
+      beforePopState: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: jest.fn(),
+      },
+      isFallback: false,
+      isPreview: false,
+    };
+  },
+}));
+
+test('RecordPatternDetailForm', () => {
+  const mockSession: Session = {
+    expires: "1",
+    user: { email: "a", name: "Delta" },
+  };
+
+  (useSession as jest.Mock).mockReturnValueOnce([mockSession, false]);
+
+  const { asFragment } = render(<RecordPatternDetailForm />);
+  expect(asFragment()).toMatchSnapshot();
+});
