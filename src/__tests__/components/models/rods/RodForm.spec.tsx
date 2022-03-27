df993c83ad37c0914440f09f1e82cca99c81b86e
keisuke <mrtambourineman8358@gmail.com>
2022-03-27 23:50:27 +0900

    rod form jest

---
+++ src/__tests__/components/models/rods/RodForm.spec.tsx
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from '../../../utils';
+import { useSession } from "next-auth/react";
+import { Session } from "next-auth";
+import RodForm from '../../../../components/model/rods/RodForm';
+
+jest.mock("next-auth/react")
+
+
+test('RodForm', () => {
+  const mockSession: Session = {
+    expires: "1",
+    user: { email: "a", name: "Delta" },
+  };
+
+  (useSession as jest.Mock).mockReturnValueOnce([mockSession, false]);
+
+  const { asFragment } = render(<RodForm />);
+  expect(asFragment()).toMatchSnapshot();
+});
