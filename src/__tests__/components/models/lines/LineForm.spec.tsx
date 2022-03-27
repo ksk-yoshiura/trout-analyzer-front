acdeac7111b45d6843cb4f54ee4aaf8bff1cca08
keisuke <mrtambourineman8358@gmail.com>
2022-03-27 23:50:38 +0900

    line form jest

---
+++ src/__tests__/components/models/lines/LineForm.spec.tsx
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from '../../../utils';
+import { useSession } from "next-auth/react";
+import { Session } from "next-auth";
+import LineForm from '../../../../components/model/lines/LineForm';
+
+jest.mock("next-auth/react")
+
+
+test('LineForm', () => {
+  const mockSession: Session = {
+    expires: "1",
+    user: { email: "a", name: "Delta" },
+  };
+
+  (useSession as jest.Mock).mockReturnValueOnce([mockSession, false]);
+
+  const { asFragment } = render(<LineForm />);
+  expect(asFragment()).toMatchSnapshot();
+});
