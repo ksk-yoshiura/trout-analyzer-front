009282b6c9f398481d9c017e121f69b586eee19e
keisuke <mrtambourineman8358@gmail.com>
2022-04-02 18:23:19 +0900

    pattern list

---
+++ src/pages/api/pattern_conditions/index.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export type PatternCondition = {
+  ID: string
+  typeName: string
+  typeNum: string
+}
+
+// API のレスポンス型
+export type PatternConditionsApiResponse = {
+  result: PatternCondition[]
+  status: number
+  message?: string
+}
+
+// API のエントリポイント
+export default function PatternConditionApi(
+  req: NextApiRequest,
+  res: NextApiResponse<PatternConditionsApiResponse>
+): void {
+}
