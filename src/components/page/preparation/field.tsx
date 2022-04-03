b731e166c34268056925b3b4230351e9fd830c45
keisuke <mrtambourineman8358@gmail.com>
2022-04-03 20:32:56 +0900

    preparation field page

--- src/components/page/preparation/field.tsx
+++ src/components/page/preparation/field.tsx
@@ -1,23 +1,6 @@
-import {
-  Flex,
-  Link
-} from '@chakra-ui/react'
-import NextLink from "next/link"
-import PreparationFieldModal from '../../model/preparation/PreparationFieldModal'
-import PreparationFieldSelect from '../../model/preparation/PreparationFieldSelect'
+import RecordStartingForm from '../../model/records/RecordStartingForm'
 
 export default function FieldIsReady(): JSX.Element {
-  // TODO：New Fieldで登録後登録されたfieldを選択状態にしたい
 
-  return (
-    <>
-      <Flex py={50} w="100wh" justifyContent={'center'}>
-        <PreparationFieldSelect />
-        <PreparationFieldModal />
-      </Flex>
-      <NextLink href="/records/serial_register" passHref>
-        <Link w='100%' as={'button'} color='teal'>Field Is Ready</Link>
-      </NextLink>
-    </>
-  );
+  return (<RecordStartingForm />);
 }
