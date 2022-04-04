823d2c6ab094061d02379c2fd3f31001866bedb8
keisuke <mrtambourineman8358@gmail.com>
2022-04-04 23:49:01 +0900

    response type of pattern index

--- src/pages/api/patterns/index.ts
+++ src/pages/api/patterns/index.ts
@@ -1,15 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export type Pattern = {
-  id: string
-  imageUrl: string
-  imageAlt: string
-  createdAt: string
-  result: string
-  lureType: string
-  weather: string
-  depth: string
-  speed: string
+  ID: string
+  CreatedAt: string
+  // バッジ情報
+  // 釣果
+  ResultCondition: {
+    typeName: string
+  },
+  SpeedCondition: {
+    typeName: string
+  },
+  DepthCondition: {
+    typeName: string
+  },
+  WeatherCondition: {
+    typeName: string
+  },
+  // ルアー情報
+  Lure: {
+    ID: string
+    LureType: {
+      typeName: string
+    }
+    name: string
+    companyName: string
+    color: string
+    weight: string
+  },
 }
 
 // API のレスポンス型
@@ -33,151 +51,151 @@ export default function PatternsApi(
 }
 
 // 擬似的なデータフェッチ関数
-function fetchPatternData(): Pattern[] | undefined {
-  const patterns: Pattern[] = [
-    {
-      id: '1',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '2',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '3',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '4',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '5',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '6',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '7',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '8',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '9',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '10',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '11',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '12',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    },
-    {
-      id: '13',
-      imageUrl: 'https://bit.ly/2Z4KKcF',
-      imageAlt: 'lure image',
-      createdAt: '11:30',
-      result: 'caught',
-      lureType: 'spoon',
-      weather: 'sunny',
-      depth: 'shallow',
-      speed: 'fast'
-    }
-  ]
-  return patterns
-}
+// function fetchPatternData(): Pattern[] | undefined {
+//   const patterns: Pattern[] = [
+//     {
+//       id: '1',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '2',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '3',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '4',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '5',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '6',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '7',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '8',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '9',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '10',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '11',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '12',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     },
+//     {
+//       id: '13',
+//       imageUrl: 'https://bit.ly/2Z4KKcF',
+//       imageAlt: 'lure image',
+//       createdAt: '11:30',
+//       result: 'caught',
+//       lureType: 'spoon',
+//       weather: 'sunny',
+//       depth: 'shallow',
+//       speed: 'fast'
+//     }
+//   ]
+//   return patterns
+// }
