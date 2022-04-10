2d34ae67e6c9c5c20635bd881e481e4e77bde396
keisuke <mrtambourineman8358@gmail.com>
2022-04-10 09:12:42 +0900

    terraform spa side

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "network" {
+  source   = "./network"
+  app_name = var.app_name
+}
+
+module "acm" {
+  source = "./acm"
+  domain = var.domain
+}
+
+module "spa" {
+  source   = "./spa"
+  app_name = var.app_name
+  domain   = var.domain
+  acm_id   = module.acm.acm_id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "app_name" {}
+
+variable "domain" {}
