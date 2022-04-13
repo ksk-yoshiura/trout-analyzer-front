3235eb5ac8e135422c6f46022e43e149e27d2487
keisuke <mrtambourineman8358@gmail.com>
2022-04-13 23:37:49 +0900

    easier to press

--- src/components/page/records/serial_register.tsx
+++ src/components/page/records/serial_register.tsx
@@ -1,11 +1,16 @@
 import RecordSerialRegisterForm from '../../model/records/RecordSerialRegisterForm'
 import { useRouter } from "next/router"
+import {
+  Flex
+} from '@chakra-ui/react'
 
 export default function SerialRegister(): JSX.Element {
   // ページ遷移
   const router = useRouter();
   const { record_id } = router.query
   return (
+    <Flex pb={20}>
       <RecordSerialRegisterForm recordId={record_id} />
+    </Flex>
   );
 }
