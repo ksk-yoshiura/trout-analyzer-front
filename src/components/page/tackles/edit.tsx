63d2ee60843f04d23e7762e275c9f7228678a11a
keisuke <mrtambourineman8358@gmail.com>
2022-04-19 23:57:49 +0900

    showing tackle edit page

--- src/components/page/tackles/edit.tsx
+++ src/components/page/tackles/edit.tsx
@@ -2,11 +2,23 @@ import {
   Flex
 } from '@chakra-ui/react'
 import TackleForm from '../../model/tackles/TackleForm'
+import { useRouter } from "next/router";
+import Loading from '../../shared/Loading'
+import useSWR from 'swr'
+import { TacklesApiResponse } from "../../../pages/api/tackles/[id]"
+
 
 export default function TackleEdit(): JSX.Element {
+  // パラメータからタックルID取得
+  const router = useRouter();
+  const { id } = router.query
+  // APIからデータ取得
+  const { data, error } = useSWR<TacklesApiResponse, Error>('tackles/' + id)
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <Loading />
   return (
     <Flex textAlign="center" w="100wh">
-      <TackleForm />
+      <TackleForm tackleData={data?.result} />
     </Flex>
   )
 }
