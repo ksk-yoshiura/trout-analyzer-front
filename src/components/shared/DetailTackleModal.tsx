1da6001241212cd52886964338abb9e426249832
keisuke <mrtambourineman8358@gmail.com>
2022-04-23 16:21:29 +0900

    detail moda lfor tackle

---
+++ src/components/shared/DetailTackleModal.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode } from 'react'
+import {
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  Modal,
+} from '@chakra-ui/react'
+
+type DetailProps = {
+  chosenId?: number
+  title: string
+  isOpen: boolean
+  onClose: any
+  mutate: any
+  children?: ReactNode;
+}
+
+export default function DetailTackleModal(props: DetailProps): JSX.Element {
+  // 各値
+  const { title, children, isOpen, onClose } = props
+  // 詳細モーダルタイトル
+  const modalTitle = title
+
+  return ( // 詳細モーダル
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader textTransform={'capitalize'}>{modalTitle + ' Detail'}</ModalHeader>
+        <ModalCloseButton />
+        {children}
+      </ModalContent>
+    </Modal>
+  )
+}
