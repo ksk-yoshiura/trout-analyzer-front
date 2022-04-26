fb6d548075f9ea69c6efcbfbacbab58d164bc95f
keisuke <mrtambourineman8358@gmail.com>
2022-04-26 23:49:22 +0900

    jets for lines

---
+++ src/__tests__/components/page/lines/edit.spec.tsx
@@ -0,0 +1,39 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from '../../../utils';
+import LineEdit from '../../../../components/page/lines/edit';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '/',
+      query: {},
+      asPath: '/',
+      basePath: '/',
+      isLocaleDomain: true,
+      isReady: true,
+      push: jest.fn(),
+      prefetch: jest.fn(),
+      replace: jest.fn(),
+      reload: jest.fn(),
+      back: jest.fn(),
+      beforePopState: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: jest.fn(),
+      },
+      isFallback: false,
+      isPreview: false,
+    };
+  },
+}));
+
+test('LineEdit', () => {
+  const { asFragment } = render(<LineEdit />);
+  expect(asFragment()).toMatchSnapshot();
+});
