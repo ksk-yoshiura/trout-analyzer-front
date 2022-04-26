54aca22541a18e1fc4143d15a0f177635417302b
keisuke <mrtambourineman8358@gmail.com>
2022-04-26 23:49:43 +0900

    jets for lures

---
+++ src/__tests__/components/page/lures/create.spec.tsx
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from '../../../utils';
+import  { useSession } from "next-auth/react";
+import  { Session } from "next-auth";
+import LureCreate from '../../../../components/page/lures/create';
+
+jest.mock("next-auth/react")
+
+test('LureCreate', () => {
+  const mockSession: Session = {
+    expires: "1",
+    user: { email: "a", name: "Delta" },
+  };
+
+  (useSession as jest.Mock).mockReturnValueOnce([mockSession, false]);
+
+  const { asFragment } = render(<LureCreate />);
+  expect(asFragment()).toMatchSnapshot();
+});
---
+++ src/__tests__/components/page/lures/edit.spec.tsx
@@ -0,0 +1,39 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render } from '../../../utils';
+import LureEdit from '../../../../components/page/lures/edit';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '/',
+      query: {},
+      asPath: '/',
+      basePath: '/',
+      isLocaleDomain: true,
+      isReady: true,
+      push: jest.fn(),
+      prefetch: jest.fn(),
+      replace: jest.fn(),
+      reload: jest.fn(),
+      back: jest.fn(),
+      beforePopState: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: jest.fn(),
+      },
+      isFallback: false,
+      isPreview: false,
+    };
+  },
+}));
+
+test('LureEdit', () => {
+  const { asFragment } = render(<LureEdit />);
+  expect(asFragment()).toMatchSnapshot();
+});
