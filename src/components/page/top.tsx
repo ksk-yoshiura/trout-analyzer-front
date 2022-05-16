abb80d6306a411d55b7084659aa88e15fb820e90
keisuke <mrtambourineman8358@gmail.com>
2022-05-17 00:14:16 +0900

    gave up statistic

--- src/components/page/top.tsx
+++ src/components/page/top.tsx
@@ -3,18 +3,21 @@ import {
   Stack,
   Button
 } from '@chakra-ui/react';
-import TopStatistic from '../model/top_partial/statistic'
 import NextLink from "next/link"
 
 export default function TopPage(): JSX.Element {
   return (
     <>
-      <TopStatistic />
 
       <Stack mt={50} spacing={10}>
         <NextLink href="/preparation/field" passHref>
           <Button color="white" bg='tomato' size='lg' w={300} p={4}>
             Get Started
+          </Button>
+        </NextLink>
+        <NextLink href="/records/all" passHref>
+          <Button color="white" bg='teal' size='lg' w={300} p={4}>
+            Check Your Records
           </Button>
         </NextLink>
       </Stack>
