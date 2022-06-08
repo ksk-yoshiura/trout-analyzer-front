3c9d4fb535d2a4069ac9d00c62ab4f0298e35c86
keisuke <mrtambourineman8358@gmail.com>
2022-06-09 00:47:23 +0900

    back to all link

--- src/components/page/records/list.tsx
+++ src/components/page/records/list.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import {
   Flex,
-  Button
+  Link,
+  Button, Icon
 } from '@chakra-ui/react'
 import NextLink from "next/link"
+import { ArrowBackIcon } from '@chakra-ui/icons'
 import { useRouter } from "next/router";
 import RecordPatternsList from '../../model/records/RecordPatternsList'
 
@@ -11,11 +13,23 @@ export default function RecordsList(): JSX.Element {
   // パラメータからレコードID取得
   const router = useRouter();
   const { record_id } = router.query
+  // 戻るリンク
+  const backLinkToRecordsAllPage = "/records/all"
+
   return (
     <>
+      <NextLink href={backLinkToRecordsAllPage} passHref>
+        <Link pb={5} as={'button'} color='teal'>
+          <Icon as={ArrowBackIcon} />
+          Back to records all
+        </Link>
+      </NextLink>
       <Flex pb={5} w="100%">
         <NextLink href={"/records/" + record_id + "/patterns/create"} passHref>
-          <Button  pl={10} pr={10} colorScheme='teal'>Add Pattern</Button>
+          <Button pl={10} pr={10} colorScheme='teal'>Add Pattern</Button>
+        </NextLink>
+        <NextLink href={"/records/" + record_id + "/patterns/analysis"} passHref>
+          <Button pl={10} ml={10} pr={10} colorScheme='teal'>Analyze Patterns</Button>
         </NextLink>
       </Flex>
       <RecordPatternsList />
