487ebf9fe60dfb01ec74ae04489e41c7e7c69411
keisuke <mrtambourineman8358@gmail.com>
2022-06-10 21:10:11 +0900

    pattern analysis

---
+++ src/components/page/records/analysis.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+  Link, 
+  Icon
+} from '@chakra-ui/react'
+import NextLink from "next/link"
+import { ArrowBackIcon } from '@chakra-ui/icons'
+import { useRouter } from "next/router";
+import RecordPatternsAnalysis from '../../model/records/RecordPatternsAnalysis'
+
+export default function PatternAnalysis() {
+  // パラメータからレコードID取得
+  const router = useRouter();
+  const { record_id } = router.query
+  // 戻るリンク
+  const backLinkToPatternListPage = "/records/" + record_id + "/patterns/list"
+  return (
+    <>
+      <NextLink href={backLinkToPatternListPage} passHref>
+        <Link pb={5} as={'button'} color='teal'>
+          <Icon as={ArrowBackIcon} />
+          Back to patterns list
+        </Link>
+      </NextLink>
+      <RecordPatternsAnalysis />
+    </>
+  )
+}
---
+++ src/pages/records/[record_id]/patterns/analysis.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import PatternAnalysis from '../../../../components/page/records/analysis';
+
+export default function Analysis(): JSX.Element {
+  return (
+    <PatternAnalysis />
+  );
+}
