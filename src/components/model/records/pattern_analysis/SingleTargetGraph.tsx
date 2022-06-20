455e6c141baad3f5303c3278af71ff5f6be5338e
keisuke <mrtambourineman8358@gmail.com>
2022-06-21 00:08:58 +0900

    looks better

--- src/components/model/records/pattern_analysis/SingleTargetGraph.tsx
+++ src/components/model/records/pattern_analysis/SingleTargetGraph.tsx
@@ -12,30 +12,36 @@ const data = {
       label: 'red',
       data: [2, 3, 4, 5, 6, 8],
       backgroundColor: 'red',
+      stack: 'Reaction',
     },
     {
       label: 'blue',
       data: [1, 3, 7, 7, 3, 8],
       backgroundColor: 'blue',
+      stack: 'Reaction',
     },
     {
       label: 'green',
       data: [1, 3, 7, 8, 9, 8],
       backgroundColor: 'green',
+      stack: 'Reaction',
     },
     {
       label: 'yellow',
-      data: [1, 0, 0, -8, -6, -8],
+      data: [1, 0, 0, 8, 6, 8],
       backgroundColor: 'yellow',
+      stack: 'No Reaction',
     },
   ]
 
 };
 
 const options = {
-  title: {
-    display: true,
-    text: 'Color - Depth'
+  plugins: {
+    title: {
+      display: true,
+      text: 'Color - Depth'
+    }
   },
   scales: {
     x: {
