395af424a86662a4add2102d8f2e0349d533de44
keisuke <mrtambourineman8358@gmail.com>
2022-06-20 23:41:14 +0900

    demo graph

--- src/components/model/records/pattern_analysis/SingleTargetGraph.tsx
+++ src/components/model/records/pattern_analysis/SingleTargetGraph.tsx
@@ -1,5 +1,5 @@
 import { Bar } from 'react-chartjs-2';
-import { CategoryScale } from 'chart.js'; 
+import { CategoryScale } from 'chart.js';
 import Chart from 'chart.js/auto';
 
 Chart.register(CategoryScale);
@@ -9,32 +9,45 @@ const data = {
   labels: ['super fast', 'fast', 'normal', 'slow', 'super slow'],
   datasets: [
     {
-      axis: 'y',
-      label: 'Dataset',
-      // データの値
-      data: [5, 9, 8, 1, 6],
-      // グラフの背景色
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-      ],
-      // グラフの枠線の色
-      borderColor: [
-        'rgb(255, 99, 132)',
-      ],
-      // グラフの枠線の太さ
-      borderWidth: 1,
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      },
-    }
-  ],
+      label: 'red',
+      data: [2, 3, 4, 5, 6, 8],
+      backgroundColor: 'red',
+    },
+    {
+      label: 'blue',
+      data: [1, 3, 7, 7, 3, 8],
+      backgroundColor: 'blue',
+    },
+    {
+      label: 'green',
+      data: [1, 3, 7, 8, 9, 8],
+      backgroundColor: 'green',
+    },
+    {
+      label: 'yellow',
+      data: [1, 0, 0, -8, -6, -8],
+      backgroundColor: 'yellow',
+    },
+  ]
+
 };
+
+const options = {
+  title: {
+    display: true,
+    text: 'Color - Depth'
+  },
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true
+    }
+  },
+}
 
 // レンダリング
 export default function SingleTargetGraph(): JSX.Element {
-  return <Bar data={data} />;
+  return <Bar data={data} options={options} />;
 }
