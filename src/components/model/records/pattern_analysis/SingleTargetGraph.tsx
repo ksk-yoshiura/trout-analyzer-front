c6351e6441e068229dcd2c71299606f889f6b361
keisuke <mrtambourineman8358@gmail.com>
2022-07-01 00:35:27 +0900

    graph data for color weather

--- src/components/model/records/pattern_analysis/SingleTargetGraph.tsx
+++ src/components/model/records/pattern_analysis/SingleTargetGraph.tsx
@@ -1,46 +1,30 @@
 import { Bar } from 'react-chartjs-2';
 import { CategoryScale } from 'chart.js';
 import Chart from 'chart.js/auto';
+import Loading from '../../../shared/Loading'
+import useSWR, { mutate } from 'swr'
+import { ColorWeatherAnalysisApiResponse } from "../../../../pages/api/patterns/analysis/color_weather/[record_id]/[result]"
 
 Chart.register(CategoryScale);
 
-const data = {
+const dataFrame = {
   // x 軸のラベル
-  labels: ['super fast', 'fast', 'normal', 'slow', 'super slow'],
+  labels: ['sunny', 'rainy', 'cloudy'],
   datasets: [
     {
-      label: 'red',
-      data: [2, 3, 4, 5, 6, 8],
-      backgroundColor: 'red',
-      stack: 'Reaction',
-    },
-    {
-      label: 'blue',
-      data: [1, 3, 7, 7, 3, 8],
-      backgroundColor: 'blue',
-      stack: 'Reaction',
-    },
-    {
-      label: 'green',
-      data: [1, 3, 7, 8, 9, 8],
-      backgroundColor: 'green',
-      stack: 'Reaction',
-    },
-    {
-      label: 'yellow',
-      data: [1, 0, 0, 8, 6, 8],
-      backgroundColor: 'yellow',
-      stack: 'No Reaction',
-    },
+      label: '',
+      data: [0,0,0], // 初期値は0
+      backgroundColor: '',
+      stack: '',
+    }
   ]
-
 };
 
 const options = {
   plugins: {
     title: {
       display: true,
-      text: 'Color - Depth'
+      text: 'Color - Weather'
     }
   },
   scales: {
@@ -53,7 +37,77 @@ const options = {
   },
 }
 
+type GraphData = {
+  label: string
+  data: Array<number>
+  backgroundColor: string
+  stack: string
+}
+
 // レンダリング
 export default function SingleTargetGraph(): JSX.Element {
-  return <Bar data={data} options={options} />;
+  // 雛形
+  const dataForm = {
+    label: '',
+    data: [0,0,0], // 初期値は0
+    backgroundColor: '',
+    stack: '',
+  }
+
+  let tempDataForm = {
+    label: '',
+    data: [0,0,0], // 初期値は0
+    backgroundColor: '',
+    stack: '',
+  }
+  // グラフで表示するデータセット
+  let tempListForGraph: Array<GraphData> = []
+  const weatherTypeList = ['sunny', 'rainy', 'cloudy'] // 条件リスト
+  // APIからデータ取得
+  const { data, error } = useSWR<ColorWeatherAnalysisApiResponse, Error>('pattern/analysis/color_weather/2/all')
+  if (error) return <p>Error: {error.message}</p>
+  if (!data) return <Loading />
+  console.log(data)
+
+  const listDataForGraph = data.result ? data.result : []
+  listDataForGraph.map((val, index) => { // APIから取得したデータ
+
+    const paraentVal = val
+    if (index === 0) { // 最初のデータを格納
+      tempDataForm.label = paraentVal.ColorName + '-' + paraentVal.ResultType
+      tempDataForm.backgroundColor = paraentVal.ColorCode
+      tempDataForm.stack = paraentVal.ResultType
+      // 初期化することで参照を切る
+      tempDataForm.data = [0,0,0]
+      const indexWeatherList = weatherTypeList.indexOf(paraentVal.WeatherType)
+      if (indexWeatherList !== -1) tempDataForm.data[indexWeatherList] = paraentVal.Sum
+      // データ加工用配列
+      tempListForGraph.push(tempDataForm)
+      return
+    }
+    const lastData = tempListForGraph.slice(-1)[0]
+    // すでに整形したデータリスト
+    // すでにあるデータ
+    if (paraentVal.ColorCode === lastData.backgroundColor && paraentVal.ResultType === lastData.stack) { // 色と結果が同じ
+      // 条件リストに該当があれば合計値を格納
+      const indexWeatherList = weatherTypeList.indexOf(paraentVal.WeatherType)
+      if (indexWeatherList !== -1) lastData.data[indexWeatherList] = paraentVal.Sum
+    } else {
+      // 以下新規
+      const newData = { ...dataForm }
+      // 条件リストに該当があれば合計値を格納
+      const indexWeatherList = weatherTypeList.indexOf(paraentVal.WeatherType)
+      // 初期化することで参照を切る
+      newData.data = [0,0,0]
+      if (indexWeatherList !== -1) newData.data[indexWeatherList] = paraentVal.Sum
+      newData.backgroundColor = paraentVal.ColorCode
+      newData.label = paraentVal.ColorName + '-' + paraentVal.ResultType
+      newData.stack = paraentVal.ResultType
+      tempListForGraph.push(newData)
+    }
+  })
+  // 上書き
+  dataFrame.datasets = tempListForGraph
+
+  return <Bar data={dataFrame} options={options} />;
 }
