f6f1018bf5ef25a355033fbc36caba55c6f7efa5
keisuke <mrtambourineman8358@gmail.com>
2022-07-01 21:54:39 +0900

    showing graph

--- src/components/model/records/RecordPatternsAnalysis.tsx
+++ src/components/model/records/RecordPatternsAnalysis.tsx
@@ -1,143 +1,28 @@
-import React from 'react'
-import {
-  Formik,
-  Form,
-  Field,
-  FieldProps,
-  useFormikContext
-} from 'formik';
-import {
-  Button,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  Stack,
-  useDisclosure,
-  Drawer,
-  DrawerBody,
-  DrawerOverlay,
-  DrawerContent
-} from "@chakra-ui/react"
+import React, { useState } from 'react'
 import ResultRadioGroup from '../records/pattern_analysis/ResultRadioGroup'
-import TargetCheckBox from '../records/pattern_analysis/TargetCheckBox'
-import SingleTargetGraph from '../records/pattern_analysis/SingleTargetGraph'
-import { createAxiosInstance } from "../../../pages/api/utils"
-
-type PatternAnalyzeProp = {
-  result: string
-  target: string
-}
-
+import ColorWeatherGraph from '../records/pattern_analysis/ColorWeatherGraph'
+import ColorDepthGraph from '../records/pattern_analysis/ColorDepthGraph'
+import ColorLureTypeGraph from '../records/pattern_analysis/ColorLureTypeGraph'
+import { useRouter } from "next/router";
 
 export default function RecordPatternAnalysis() {
-  // 確認ドロワー
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  // パラメータからレコードID取得
+  const router = useRouter();
+  const { record_id } = router.query
 
-  function handlePatternAnalyze(values: PatternAnalyzeProp) {
-    console.log(values)
-
-  }
-  function validateData(value: any) {
-  }
-
-  // 確認ドロワー
-  const ConfirmDrawer = () => {
-
-    // サブミット
-    const { submitForm } = useFormikContext();
-    return (
-      <Drawer placement={'bottom'} onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay />
-        <DrawerContent h={'30vh'}>
-          <DrawerBody mt={10} display={'flex'} justifyContent={'space-around'}>
-            <Button
-              onClick={onClose}
-              colorScheme='gray'
-              variant='solid'
-            >Cancel</Button>
-            <Button
-              type="submit"
-              onClick={() => { submitForm(), onClose() }}
-              colorScheme='teal'
-              variant='solid'
-            >Confirm</Button>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    )
-  }
+  const [targetResult, setTargetResult] = useState('all')
 
   return (
     <>
-
-      <Formik
-        initialValues={{
-          result: '',
-          target: ''
-
-        }}
-        onSubmit={(values, actions) => {
-          setTimeout(() => {
-            handlePatternAnalyze(values)
-            actions.setSubmitting(false)
-          }, 1000)
-        }}
-      >
-        {(props) => (
-          <Form>
-            <Stack spacing={5}>
-
-              <Field name='result' validate={validateData}>
-                {({ field, form }: FieldProps) => (
-                  <FormControl
-                    isInvalid={Boolean(form.errors.result)
-                      && Boolean(form.touched.result)}
-                  >
-                    <FormLabel
-                      fontSize="12px"
-                      htmlFor='result'
-                      textTransform='uppercase'
-                    >Select Result</FormLabel>
-                    <ResultRadioGroup field={field} />
-                    <FormErrorMessage>{form.errors.result}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-
-              <Field name='target' type='checkbox' validate={validateData}>
-                {({ field, form }: FieldProps) => (
-                  <FormControl
-                    isInvalid={Boolean(form.errors.target)
-                      && Boolean(form.touched.target)}
-                  >
-                    <FormLabel
-                      fontSize="12px"
-                      htmlFor='target'
-                      textTransform='uppercase'
-                    >Select Target</FormLabel>
-                    <TargetCheckBox field={field} />
-                    <FormErrorMessage>{form.errors.target}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-
-            </Stack>
-            <Button
-              mt={4}
-              mb={10}
-              width={"100%"}
-              colorScheme='teal'
-              type='button'
-              onClick={onOpen}
-            >
-              Analyze
-            </Button>
-            <ConfirmDrawer />
-          </Form>
-        )}
-      </Formik>
-
-      <SingleTargetGraph />
+      <ResultRadioGroup setTargetResult={setTargetResult}  />
+    {
+      record_id? <>
+      <ColorWeatherGraph recordId={record_id} targetParam={targetResult} />
+      <ColorDepthGraph recordId={record_id} targetParam={targetResult} />
+      <ColorLureTypeGraph recordId={record_id} targetParam={targetResult} />
+      </>
+      :<></>
+    }
     </>
   )
 }
