fd339e198f3ba9d5433eb54f20c5e4623838ed23
keisuke <mrtambourineman8358@gmail.com>
2022-07-11 23:52:25 +0900

    function replaced by const according to eslint

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,7 +1,9 @@
-export function getDateFormatted(value: string) {
+const getDateFormatted = (value: string): string => {
   const date = new Date(value);
   const day = ('0' + date.getDate()).slice(-2);
   const month = ('0' + (date.getMonth() + 1)).slice(-2);
   const year = date.getFullYear();
   return year + '-' + month + '-' + day;
 }
+
+export default getDateFormatted
