7dbac542484ddfda46658010098f752163871e4b
keisuke <mrtambourineman8358@gmail.com>
2022-09-23 01:15:56 +0900

    login trial

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,36 +1,20 @@
-import {
-  useToast
-} from "@chakra-ui/react";
-
 import { CreateAxiosDefaultInstance } from "../pages/api/utils"
-
 
 export default function Hello() {
   // APIからデータ取得
   const axiosInstance = CreateAxiosDefaultInstance()
 
-  // アラート
-  const toast = useToast()
-  axiosInstance.get('/')
-    .then((res) => {
-      console.log(res)
-      // アラート代わりにトーストを使用
-      toast({
-        title: 'Lure updated!',
-        description: "We've updated your lure data for you.",
-        status: 'success',
-        duration: 9000,
-        isClosable: true,
-      })
+  const postData = {
+    mailaddress: "gogo123@example.com",
+    password: "testpass",
+
+  }
+  axiosInstance.post('login', postData)
+    .then((v) => {
+      return v.data.token
     })
     .catch((error) => {
-      toast({
-        title: 'Failed!',
-        description: error.message,
-        status: 'error',
-        duration: 9000,
-        isClosable: true,
-      })
+      console.log(error)
     })
 
   return (
