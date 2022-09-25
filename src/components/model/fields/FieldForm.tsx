b566d5bfef0bfd81fc4249b6ead6bf3f0cbed3e5
keisuke <mrtambourineman8358@gmail.com>
2022-09-26 00:36:47 +0900

    async await

--- src/components/model/fields/FieldForm.tsx
+++ src/components/model/fields/FieldForm.tsx
@@ -56,9 +56,9 @@ export default function FieldForm(props: DetailProps) {
   const axiosInstance = CreateAxiosInstance()
 
   // API登録・更新
-  const handleSendFieldData = (values: FieldData) => {
+  const handleSendFieldData = async (values: FieldData) => {
     // 画像データはbase64に変換
-    const imageBase64 = values.image ? convertFileIntoBase64(values.image) : ''
+    const imageBase64 = values.image ? await convertFileIntoBase64(values.image) : ''
     console.log(values.image)
     console.log(imageBase64)
     const fieldPostData = {
